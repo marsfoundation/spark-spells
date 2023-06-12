05c06fe472921738ba3a18d9017a3633fa7df675
lucas-manuel <lucasmanuel.tech@gmail.com>
2023-06-12 14:56:50 +0200

    feat: working script

--- tenderly.js
+++ tenderly.js
@@ -18,6 +18,7 @@ async function mainnetFork() {
         chain_id: 11,
         shanghai_time: 1677557088,
       },
+      name: "Mainnet Fork" + Date.now(),
     },
     {
       headers: {
@@ -29,78 +30,44 @@ async function mainnetFork() {
 
 
 const runSpell = async () => {
-  console.time('Fork Creation');
   const fork = await mainnetFork();
-  console.timeEnd('Fork Creation');
 
   const forkId = fork.data.simulation_fork.id;
   const rpcUrl = `https://rpc.tenderly.co/fork/${forkId}`;
-  // const rpcUrl = 'https://rpc.tenderly.co/fork/###-###-###-###';
+
   console.log('Fork URL\n\t' + rpcUrl);
 
   const forkProvider = new ethers.providers.JsonRpcProvider(rpcUrl);
 
-  const PAUSE_PROXY = 0xBE8E3e3618f7474F8cB1d074A26afFef007E98FB;
-  const EXECUTOR = 0x3300f198988e4C9C63F75dF86De36421f06af8c4;
+  const EXECUTOR    = "0x3300f198988e4C9C63F75dF86De36421f06af8c4";
+  const ACL_MANAGER = "0xdA135Cd78A086025BcdC87B038a1C462032b510C"
+  const PAUSE_PROXY = "0xBE8E3e3618f7474F8cB1d074A26afFef007E98FB";
+  const PAYLOAD     = "0x41D7c79aE5Ecba7428283F66998DedFD84451e0e";  // TODO: Maybe Deploy as part of this script
 
-  const pauseProxySigner = forkProvider.getSigner(PAUSE_PROXY)
-
-  // -------- This code doesn't work -----------
-
-  const ownerAddress = '0xdc6bdc37b2714ee601734cf55a05625c9e512461';
+  const pauseProxySigner = forkProvider.getSigner(PAUSE_PROXY);
 
   await pauseProxySigner.sendTransaction({
     from: PAUSE_PROXY,
-    // to: EXECUTOR,
-    to: "0x6b175474e89094c44da98b954eedeac495271d0f",
-    data: DaiAbi.encodeFunctionData('mint', [
-      ethers.utils.hexZeroPad(ownerAddress.toLowerCase(), 20),
-      ethers.utils.parseEther('1.0'),
-    ]),
-    gasLimit: 800000,
+    to: ACL_MANAGER,
+    data: AclManagerAbi.encodeFunctionData('addPoolAdmin', [EXECUTOR]),
   });
 
-  // -------- This code doesn't work -----------
-
-  // -------- This code works (from example in docs) -----------
-
-  // // This is from the example code and works
-  // const [minterAddress, ownerAddress, spenderAddress, receiverAddress] =
-  //   await forkProvider.listAccounts();
-
-  // const [minterSigner, ownerSigner, spenderSigner] = [
-  //   forkProvider.getSigner(minterAddress),
-  //   forkProvider.getSigner(ownerAddress),
-  //   forkProvider.getSigner(spenderAddress),
-  //   forkProvider.getSigner(receiverAddress),
-  // ];
-
-  // await minterSigner.sendTransaction({
-  //   from: minterAddress,
-  //   to: '0x6b175474e89094c44da98b954eedeac495271d0f',
-  //   data: DaiAbi.encodeFunctionData('mint', [
-  //     ethers.utils.hexZeroPad(ownerAddress.toLowerCase(), 20),
-  //     ethers.utils.parseEther('1.0'),
-  //   ]),
-  //   gasLimit: 800000,
-  // });
-
-  // -------- This code works -----------
+  await pauseProxySigner.sendTransaction({
+    from: PAUSE_PROXY,
+    to: EXECUTOR,
+    data: ExecutorAbi.encodeFunctionData('exec', [
+      ethers.utils.hexZeroPad(PAYLOAD.toLowerCase(), 20),
+      PayloadAbi.encodeFunctionData('execute', [])
+    ]),
+  });
 };
 
-const DaiAbi = new ethers.utils.Interface([
-  {
-    constant: false,
-    inputs: [
-      { internalType: 'address', name: 'usr', type: 'address' },
-      { internalType: 'uint256', name: 'wad', type: 'uint256' },
-    ],
-    name: 'mint',
-    outputs: [],
-    payable: false,
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
+const ExecutorAbi = new ethers.utils.Interface([
+  "function exec(address target, bytes calldata args)"
 ]);
+
+const PayloadAbi = new ethers.utils.Interface(["function execute()"]);
+
+const AclManagerAbi = new ethers.utils.Interface(["function addPoolAdmin(address admin)"]);
 
 runSpell();
