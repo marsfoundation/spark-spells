7357800e4cbfa3bc304192974a4f1978f7c00fa2
Lucas Manuel <lucasmanuel.tech@gmail.com>
2023-06-14 10:00:13 +0200

    chore: Add script to run deployed spell on generated tenderly fork (#7)
    * feat: add working code
    
    * feat: add working code
    
    * feat: update to highlight broken code
    
    * feat: working script
    
    * ci: ad action
    
    * ci: update conditions
    
    * fix: update indentation
    
    * feat: add push condition
    
    * fix: install mocules
    
    * fix: update names
    
    * feat: update to echo rpc
    
    * ci: update github actions
    
    * fix: last cleanup
    
    * fix: rm fork from title
    
    * fix: move payload, add spell name
    
    * feat: add env var to exapmle
    
    * fix: add todo
    
    * feat: add env vars and url logging
    
    * some modifications to how tenderly fork works
    
    ---------
    
    Co-authored-by: Sam MacPherson <xylom44@gmail.com>

---
+++ tenderly.js
@@ -0,0 +1,89 @@
+const axios = require('axios');
+const dotenv = require('dotenv');
+const ethers = require('ethers');
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+const {
+  ETHERSCAN_API_KEY,
+  TENDERLY_USER,
+  TENDERLY_PROJECT,
+  TENDERLY_ACCESS_KEY,
+  EXECUTOR,
+  ACL_MANAGER,
+  PAUSE_PROXY,
+} = process.env;
+
+const payload = process.argv[2];
+
+const getContractName = async (_payload) => {
+  try {
+    const response = await axios.get(
+      `https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${_payload}&apikey=${ETHERSCAN_API_KEY}`
+    );
+
+    return response.data.result[0].ContractName;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+async function mainnetFork(_payload) {
+  return await axios.post(
+    `https://api.tenderly.co/api/v1/account/${TENDERLY_USER}/project/${TENDERLY_PROJECT}/fork`,
+    {
+      network_id: '1',
+      chain_config: {
+        chain_id: 11,
+        shanghai_time: 1677557088,
+      },
+      alias: await getContractName(_payload),
+    },
+    {
+      headers: {
+        'X-Access-Key': TENDERLY_ACCESS_KEY,
+      },
+    }
+  );
+}
+
+const runSpell = async () => {
+  const fork = await mainnetFork(payload);
+
+  const forkId = fork.data.simulation_fork.id;
+  const rpcUrl = `https://rpc.tenderly.co/fork/${forkId}`;
+
+  const forkUrl = `https://dashboard.tenderly.co/${TENDERLY_USER}/${TENDERLY_PROJECT}/fork/${forkId}`
+
+  console.log("Fork URL:", forkUrl);
+
+  const forkProvider = new ethers.providers.JsonRpcProvider(rpcUrl);
+
+  const pauseProxySigner = forkProvider.getSigner(PAUSE_PROXY);
+
+  // TODO: This can be removed after the next spell
+  await pauseProxySigner.sendTransaction({
+    from: PAUSE_PROXY,
+    to: ACL_MANAGER,
+    data: AclManagerAbi.encodeFunctionData('addPoolAdmin', [EXECUTOR]),
+  });
+
+  await pauseProxySigner.sendTransaction({
+    from: PAUSE_PROXY,
+    to: EXECUTOR,
+    data: ExecutorAbi.encodeFunctionData('exec', [
+      ethers.utils.hexZeroPad(payload.toLowerCase(), 20),
+      PayloadAbi.encodeFunctionData('execute', [])
+    ]),
+  });
+};
+
+const ExecutorAbi = new ethers.utils.Interface([
+  "function exec(address target, bytes calldata args)"
+]);
+
+const PayloadAbi = new ethers.utils.Interface(["function execute()"]);
+
+const AclManagerAbi = new ethers.utils.Interface(["function addPoolAdmin(address admin)"]);
+
+runSpell();
