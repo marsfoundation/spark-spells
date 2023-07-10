92107dbe6f448c3ab4e6bbe9a7bd0721569d40c2
lucas-manuel <lucasmanuel.tech@gmail.com>
2023-07-10 11:02:42 -0400

    feat: use new configs and new assets

--- diffs/pre-Spark-Ethereum-EMode-20230802_post-Spark-Ethereum-EMode-20230802.md
+++ diffs/pre-Spark-Ethereum-EMode-20230802_post-Spark-Ethereum-EMode-20230802.md
@@ -6,7 +6,7 @@
 
 | description | value before | value after |
 | --- | --- | --- |
-| eModeCategory | 1 | 2 |
+| eModeCategory | 1 | 3 |
 
 
 #### sDAI ([0x83F20F44975D03b1b09e64809B757c47f942BEeA](https://etherscan.io/address/0x83F20F44975D03b1b09e64809B757c47f942BEeA))
@@ -23,13 +23,6 @@
 | eModeCategory | 1 | 2 |
 
 
-#### rETH ([0xae78736Cd615f374D3085123A210448E74Fc6393](https://etherscan.io/address/0xae78736Cd615f374D3085123A210448E74Fc6393))
-
-| description | value before | value after |
-| --- | --- | --- |
-| eModeCategory | 1 | 2 |
-
-
 ## Raw diff
 
 ```json
@@ -39,10 +32,21 @@
       "from": null,
       "to": {
         "eModeCategory": 2,
-        "label": "sDAI allowed",
+        "label": "sDAI allowed - WETH",
+        "liquidationBonus": 10500,
+        "liquidationThreshold": 8251,
+        "ltv": 8001,
+        "priceSource": "0x0000000000000000000000000000000000000000"
+      }
+    },
+    "3": {
+      "from": null,
+      "to": {
+        "eModeCategory": 3,
+        "label": "sDAI allowed - wstETH",
         "liquidationBonus": 10700,
-        "liquidationThreshold": 8300,
-        "ltv": 6850,
+        "liquidationThreshold": 7951,
+        "ltv": 6851,
         "priceSource": "0x0000000000000000000000000000000000000000"
       }
     }
@@ -51,7 +55,7 @@
     "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0": {
       "eModeCategory": {
         "from": 1,
-        "to": 2
+        "to": 3
       }
     },
     "0x83F20F44975D03b1b09e64809B757c47f942BEeA": {
@@ -61,12 +65,6 @@
       }
     },
     "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2": {
-      "eModeCategory": {
-        "from": 1,
-        "to": 2
-      }
-    },
-    "0xae78736Cd615f374D3085123A210448E74Fc6393": {
       "eModeCategory": {
         "from": 1,
         "to": 2
--- src/proposals/20230802/SparkEthereum_20230802.sol
+++ src/proposals/20230802/SparkEthereum_20230802.sol
@@ -13,31 +13,47 @@ import { SparkPayloadEthereum, IEngine, Rates, EngineFlags } from '../../SparkPa
  */
 contract SparkEthereum_20230802 is SparkPayloadEthereum {
 
-    string public constant EMODE_LABEL       = 'sDAI allowed';
-    uint16 public constant EMODE_LTV         = 68_50;
-    uint16 public constant EMODE_LT          = 83_00;   // 79_50
-    uint16 public constant EMODE_LBONUS      = 107_00;  // 7%
-    uint8  public constant EMODE_CATEGORY_ID = 2;
+    string public constant EMODE_LABEL_WETH   = 'sDAI allowed - WETH';
+    string public constant EMODE_LABEL_WSTETH = 'sDAI allowed - wstETH';
 
-    address public constant RETH   = 0xae78736Cd615f374D3085123A210448E74Fc6393;
+    uint16 public constant EMODE_LTV_WETH   = 80_01;
+    uint16 public constant EMODE_LTV_WSTETH = 68_51;
+
+    uint16 public constant EMODE_LT_WSTETH = 79_51;
+    uint16 public constant EMODE_LT_WETH   = 82_51;
+
+    uint16 public constant EMODE_LBONUS_WETH   = 105_00;  // 5%
+    uint16 public constant EMODE_LBONUS_WSTETH = 107_00;  // 7%
+
+    uint8 public constant EMODE_CATEGORY_ID_WETH   = 2;
+    uint8 public constant EMODE_CATEGORY_ID_WSTETH = 3;
+
     address public constant SDAI   = 0x83F20F44975D03b1b09e64809B757c47f942BEeA;
     address public constant WETH   = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address public constant WSTETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
 
     function _postExecute() internal override {
-      LISTING_ENGINE.POOL_CONFIGURATOR().setEModeCategory(
-          EMODE_CATEGORY_ID,
-          EMODE_LTV,
-          EMODE_LT,
-          EMODE_LBONUS,
-          address(0),
-          EMODE_LABEL
-      );
+		LISTING_ENGINE.POOL_CONFIGURATOR().setEModeCategory(
+			EMODE_CATEGORY_ID_WETH,
+			EMODE_LTV_WETH,
+			EMODE_LT_WETH,
+			EMODE_LBONUS_WETH,
+			address(0),
+			EMODE_LABEL_WETH
+		);
 
-      LISTING_ENGINE.POOL_CONFIGURATOR().setAssetEModeCategory(RETH,   EMODE_CATEGORY_ID);
-      LISTING_ENGINE.POOL_CONFIGURATOR().setAssetEModeCategory(SDAI,   EMODE_CATEGORY_ID);
-      LISTING_ENGINE.POOL_CONFIGURATOR().setAssetEModeCategory(WETH,   EMODE_CATEGORY_ID);
-      LISTING_ENGINE.POOL_CONFIGURATOR().setAssetEModeCategory(WSTETH, EMODE_CATEGORY_ID);
+		LISTING_ENGINE.POOL_CONFIGURATOR().setEModeCategory(
+			EMODE_CATEGORY_ID_WSTETH,
+			EMODE_LTV_WSTETH,
+			EMODE_LT_WSTETH,
+			EMODE_LBONUS_WSTETH,
+			address(0),
+			EMODE_LABEL_WSTETH
+		);
+
+		LISTING_ENGINE.POOL_CONFIGURATOR().setAssetEModeCategory(SDAI,   EMODE_CATEGORY_ID_WETH);
+		LISTING_ENGINE.POOL_CONFIGURATOR().setAssetEModeCategory(WETH,   EMODE_CATEGORY_ID_WETH);
+		LISTING_ENGINE.POOL_CONFIGURATOR().setAssetEModeCategory(WSTETH, EMODE_CATEGORY_ID_WSTETH);
     }
 
 }
