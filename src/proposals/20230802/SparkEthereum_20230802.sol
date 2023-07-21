6939ec316e44851a831d21c21d22d9c202f5392a
lucas-manuel <lucasmanuel.tech@gmail.com>
2023-07-21 13:30:26 +0200

    fix: update formatting

--- src/proposals/20230802/SparkEthereum_20230802.sol
+++ src/proposals/20230802/SparkEthereum_20230802.sol
@@ -9,61 +9,57 @@ import { console2 as console } from "forge-std/console2.sol";
  * @title  August 2, 2023 Spark Ethereum Proposal - Remove DAI Collateral and update WETH params.
  * @author Phoenix Labs
  * @dev    This proposal removes DAI as collateral and updates WETH reserve factor and
- * 		   interest rate strategy.
+ *         interest rate strategy.
  *         TODO: Add links for forum and vote.
  */
 contract SparkEthereum_20230802 is SparkPayloadEthereum {
 
     address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-	address public constant DAI  = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public constant DAI  = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
 
-	address public constant DAI_INTEREST_RATE_STRATEGY = 0x191E97623B1733369290ee5d018d0B068bc0400D;
+    address public constant DAI_INTEREST_RATE_STRATEGY = 0x191E97623B1733369290ee5d018d0B068bc0400D;
 
-	function borrowsUpdates()
+    function borrowsUpdates()
         public pure override returns (IEngine.BorrowUpdate[] memory)
-	{
-		IEngine.BorrowUpdate[] memory borrowsUpdate = new IEngine.BorrowUpdate[](1);
+    {
+        IEngine.BorrowUpdate[] memory borrowsUpdate = new IEngine.BorrowUpdate[](1);
 
-		borrowsUpdate[0] = IEngine.BorrowUpdate({
-			asset:                 WETH,
-			reserveFactor:         5_00,
-			enabledToBorrow:       EngineFlags.KEEP_CURRENT,
-			flashloanable:         EngineFlags.KEEP_CURRENT,
-			stableRateModeEnabled: EngineFlags.KEEP_CURRENT,
-			borrowableInIsolation: EngineFlags.KEEP_CURRENT,
-			withSiloedBorrowing:   EngineFlags.KEEP_CURRENT
-		});
+        borrowsUpdate[0] = IEngine.BorrowUpdate({
+            asset:                 WETH,
+            reserveFactor:         5_00,
+            enabledToBorrow:       EngineFlags.KEEP_CURRENT,
+            flashloanable:         EngineFlags.KEEP_CURRENT,
+            stableRateModeEnabled: EngineFlags.KEEP_CURRENT,
+            borrowableInIsolation: EngineFlags.KEEP_CURRENT,
+            withSiloedBorrowing:   EngineFlags.KEEP_CURRENT
+        });
 
-		return borrowsUpdate;
-	}
+        return borrowsUpdate;
+    }
 
-	function rateStrategiesUpdates()
+    function rateStrategiesUpdates()
         public view override returns (IEngine.RateStrategyUpdate[] memory)
     {
         IEngine.RateStrategyUpdate[] memory ratesUpdate = new IEngine.RateStrategyUpdate[](1);
 
         Rates.RateStrategyParams memory weth = LISTING_ENGINE
-			.RATE_STRATEGIES_FACTORY()
-			.getStrategyDataOfAsset(WETH);
+            .RATE_STRATEGIES_FACTORY()
+            .getStrategyDataOfAsset(WETH);
 
-		console.log("weth.variableRateSlope1: %s", weth.variableRateSlope1);
-		console.log("weth.variableRateSlope2: %s", weth.variableRateSlope2);
-		console.log("weth.optimalUsageRatio:  %s", weth.optimalUsageRatio);
+        weth.variableRateSlope1 = _bpsToRay(3_00);
 
-		weth.variableRateSlope1 = _bpsToRay(3_00);
-
-		ratesUpdate[0] = IEngine.RateStrategyUpdate({
-			asset:  WETH,
-			params: weth
+        ratesUpdate[0] = IEngine.RateStrategyUpdate({
+            asset:  WETH,
+            params: weth
         });
 
         return ratesUpdate;
     }
 
     function _postExecute() internal override {
-		LISTING_ENGINE.POOL_CONFIGURATOR().configureReserveAsCollateral(DAI, 1, 1, 104_50);
+        LISTING_ENGINE.POOL_CONFIGURATOR().configureReserveAsCollateral(DAI, 1, 1, 104_50);
 
-		LISTING_ENGINE.POOL_CONFIGURATOR().setReserveInterestRateStrategyAddress(
+        LISTING_ENGINE.POOL_CONFIGURATOR().setReserveInterestRateStrategyAddress(
             DAI,
             DAI_INTEREST_RATE_STRATEGY
         );
--- src/proposals/20230802/SparkGoerli_20230802.sol
+++ src/proposals/20230802/SparkGoerli_20230802.sol
@@ -7,57 +7,57 @@ import { SparkPayloadGoerli, IEngine, Rates, EngineFlags } from '../../SparkPayl
  * @title  August 2, 2023 Spark Goerli Proposal - Remove DAI Collateral and update WETH params.
  * @author Phoenix Labs
  * @dev    This proposal removes DAI as collateral and updates WETH reserve factor and
- * 		   interest rate strategy.
+ *         interest rate strategy.
  *         TODO: Add links for forum and vote.
  */
 contract SparkGoerli_20230802 is SparkPayloadGoerli {
 
-	address public constant DAI  = 0x11fE4B6AE13d2a6055C8D9cF65c55bac32B5d844;
+    address public constant DAI  = 0x11fE4B6AE13d2a6055C8D9cF65c55bac32B5d844;
     address public constant WETH = 0x7D5afF7ab67b431cDFA6A94d50d3124cC4AB2611;
 
-	address public constant DAI_INTEREST_RATE_STRATEGY = 0x7f44e1c1dE70059D7cc483378BEFeE2a030CE247;
+    address public constant DAI_INTEREST_RATE_STRATEGY = 0x7f44e1c1dE70059D7cc483378BEFeE2a030CE247;
 
-	function borrowsUpdates()
+    function borrowsUpdates()
         public pure override returns (IEngine.BorrowUpdate[] memory)
-	{
-		IEngine.BorrowUpdate[] memory borrowsUpdate = new IEngine.BorrowUpdate[](1);
+    {
+        IEngine.BorrowUpdate[] memory borrowsUpdate = new IEngine.BorrowUpdate[](1);
 
-		borrowsUpdate[0] = IEngine.BorrowUpdate({
-			asset:                 WETH,
-			reserveFactor:         5_00,
-			enabledToBorrow:       EngineFlags.KEEP_CURRENT,
-			flashloanable:         EngineFlags.KEEP_CURRENT,
-			stableRateModeEnabled: EngineFlags.KEEP_CURRENT,
-			borrowableInIsolation: EngineFlags.KEEP_CURRENT,
-			withSiloedBorrowing:   EngineFlags.KEEP_CURRENT
-		});
+        borrowsUpdate[0] = IEngine.BorrowUpdate({
+            asset:                 WETH,
+            reserveFactor:         5_00,
+            enabledToBorrow:       EngineFlags.KEEP_CURRENT,
+            flashloanable:         EngineFlags.KEEP_CURRENT,
+            stableRateModeEnabled: EngineFlags.KEEP_CURRENT,
+            borrowableInIsolation: EngineFlags.KEEP_CURRENT,
+            withSiloedBorrowing:   EngineFlags.KEEP_CURRENT
+        });
 
-		return borrowsUpdate;
-	}
+        return borrowsUpdate;
+    }
 
-	function rateStrategiesUpdates()
+    function rateStrategiesUpdates()
         public view override returns (IEngine.RateStrategyUpdate[] memory)
     {
         IEngine.RateStrategyUpdate[] memory ratesUpdate = new IEngine.RateStrategyUpdate[](1);
 
         Rates.RateStrategyParams memory weth = LISTING_ENGINE
-			.RATE_STRATEGIES_FACTORY()
-			.getStrategyDataOfAsset(WETH);
+            .RATE_STRATEGIES_FACTORY()
+            .getStrategyDataOfAsset(WETH);
 
-		weth.variableRateSlope1 = _bpsToRay(3_00);
+        weth.variableRateSlope1 = _bpsToRay(3_00);
 
-		ratesUpdate[0] = IEngine.RateStrategyUpdate({
-			asset:  WETH,
-			params: weth
+        ratesUpdate[0] = IEngine.RateStrategyUpdate({
+            asset:  WETH,
+            params: weth
         });
 
         return ratesUpdate;
     }
 
     function _postExecute() internal override {
-		LISTING_ENGINE.POOL_CONFIGURATOR().configureReserveAsCollateral(DAI, 1, 1, 104_50);
+        LISTING_ENGINE.POOL_CONFIGURATOR().configureReserveAsCollateral(DAI, 1, 1, 104_50);
 
-		LISTING_ENGINE.POOL_CONFIGURATOR().setReserveInterestRateStrategyAddress(
+        LISTING_ENGINE.POOL_CONFIGURATOR().setReserveInterestRateStrategyAddress(
             DAI,
             DAI_INTEREST_RATE_STRATEGY
         );
