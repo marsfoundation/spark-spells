ab0abd7457ef0fb67e12015481f2afde3f2608a6
Sam MacPherson <xylom44@gmail.com>
2023-08-17 13:13:47 -0400

    [SC-42] August 16 spell (#13)
    * feat: add files
    
    * feat: setup initial infra for spell
    
    * fix: cleanup
    
    * feat: get emode category and assignment working:
    
    * feat: tests working
    
    * feat: use new configs and new assets
    
    * feat: mainnet spell tests working for new dai strategy
    
    * feat: lower WETH optimal borrow rate and reserve factor
    
    * feat: add logic to offboard DAI as collateral
    
    * feat: add back all assertions
    
    * feat: update to get e2e tests to work
    
    * feat: add additional assertions and cleanup
    
    * fix: rm diffing and update test names
    
    * feat: update rates
    
    * fix: revert rates change
    
    * fix: update to 3%
    
    * feat: update rates and logs
    
    * chore: cleanup
    
    * chore: cleanup 2
    
    * chore: cleanup 3
    
    * feat: add more rates assertions
    
    * fix: update formatting
    
    * fix: update tests
    
    * fix: update alignment issues
    
    * fix: update tabs and log
    
    * [SC-76] Adding a bunch of LTV/LT tests and increasing to 1% instead of 1bps (#11)
    
    * change ltv/lt to 1%; add extensive testing on liquidations for LTV/LT changes
    
    * spacing
    
    * spacing
    
    * Update src/proposals/20230802/SparkEthereum_20230802.t.sol
    
    Co-authored-by: Lucas Manuel <lucasmanuel.tech@gmail.com>
    
    * fixes from review
    
    * Update src/proposals/20230802/SparkEthereum_20230802.t.sol
    
    Co-authored-by: Lucas Manuel <lucasmanuel.tech@gmail.com>
    
    * got tests working
    
    ---------
    
    Co-authored-by: Lucas Manuel <lucasmanuel.tech@gmail.com>
    
    * fix: update to use 1bps
    
    * fix: update links
    
    * fix: add mainnet spells
    
    * fix: update oracle
    
    * standardize diff names
    
    * archive existing spells
    
    * update deps
    
    * remove dss-interfaces
    
    * fix breaking changes from aave-helpers
    
    * refactor to put the common logic in the base contracts
    
    * add bytecode check
    
    * fix the report diffing; add goerli sample spell
    
    * sync lib
    
    * Update src/SparkTestBase.sol
    
    Co-authored-by: Lucas Manuel <lucasmanuel.tech@gmail.com>
    
    * review changes
    
    * fix comment
    
    * remove Aug 16 spell
    
    * add aug 16 spell
    
    * add sDAI unfreeze and DAI market revert to old IRS
    
    * feat: update to work with new spell infra
    
    * update aave-helpers to latest; add spell deploys
    
    * remove unused code
    
    * add specific tests for the changes
    
    * feat: update subs
    
    * feat: update ltv calculation
    
    * update aave-helpers
    
    * test for the implementation change
    
    ---------
    
    Co-authored-by: lucas-manuel <lucasmanuel.tech@gmail.com>

---
+++ src/proposals/20230816/SparkEthereum_20230816.t.sol
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.10;
+
+import '../../SparkTestBase.sol';
+
+import { Ownable } from 'aave-v3-core/contracts/dependencies/openzeppelin/contracts/Ownable.sol';
+import { BaseAdminUpgradeabilityProxy } from 'aave-v3-core/contracts/dependencies/openzeppelin/upgradeability/BaseAdminUpgradeabilityProxy.sol';
+
+import { SparkEthereum_20230816 } from './SparkEthereum_20230816.sol';
+
+contract SparkEthereum_20230816Test is SparkEthereumTestBase {
+
+    address constant internal PAUSE_PROXY = 0xBE8E3e3618f7474F8cB1d074A26afFef007E98FB;
+
+    address public constant MCD_VAT = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
+    address public constant MCD_JUG = 0x19c0976f590D67707E62397C87829d896Dc0f1F1;
+    address public constant MCD_POT = 0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7;
+
+    address public constant DAI_INTEREST_RATE_STRATEGY_OLD
+        = 0x191E97623B1733369290ee5d018d0B068bc0400D;
+
+    address public constant DAI_INTEREST_RATE_STRATEGY_NEW
+        = 0x9f9782880dd952F067Cad97B8503b0A3ac0fb21d;
+
+    bytes32 public constant SPARK_ILK = "DIRECT-SPARK-DAI";
+
+    IPool public constant POOL = IPool(0xC13e21B648A5Ee794902342038FF3aDAB66BE987);
+
+    address public constant POOL_IMPLEMENTATION_OLD = 0x62DA45546A0F87b23941FFE5CA22f9D2A8fa7DF3;
+    address public constant POOL_IMPLEMENTATION_NEW = 0x8115366Ca7Cf280a760f0bC0F6Db3026e2437115;
+
+    constructor() {
+        id = '20230816';
+    }
+
+    function setUp() public {
+        vm.createSelectFork(getChain('mainnet').rpcUrl, 17_892_780);
+        payload = 0x60cC45DaB5F0B17789C77d5FE990f1aD80e9DD65;
+
+        // This will be done in the main spell (simulate it here)
+        loadPoolContext(poolAddressesProviderRegistry.getAddressesProvidersList()[0]);
+        vm.prank(PAUSE_PROXY);
+        Ownable(address(poolAddressesProvider)).transferOwnership(address(executor));
+    }
+
+    function testSpellSpecifics() public {
+
+        /*************************/
+        /*** Before Assertions ***/
+        /*************************/
+
+        ReserveConfig[] memory configsBefore = createConfigurationSnapshot('', POOL);
+
+        _validateDaiJugInterestRateStrategy(
+            _findReserveConfigBySymbol(configsBefore, 'DAI').interestRateStrategy,
+            DAI_INTEREST_RATE_STRATEGY_OLD,
+            DaiJugInterestStrategyValues({
+                vat:                MCD_VAT,
+                jug:                MCD_JUG,
+                ilk:                SPARK_ILK,
+                baseRateConversion: 1e27,
+                borrowSpread:       0,
+                supplySpread:       0,
+                maxRate:            0.75e27,
+                performanceBonus:   0
+            })
+        );
+        assertEq(_findReserveConfigBySymbol(configsBefore, 'sDAI').isFrozen, true);
+        vm.prank(address(poolAddressesProvider));
+        assertEq(BaseAdminUpgradeabilityProxy(payable(address(pool))).implementation(), POOL_IMPLEMENTATION_OLD);
+
+        /***********************/
+        /*** Execute Payload ***/
+        /***********************/
+
+        GovHelpers.executePayload(vm, payload, executor);
+
+        /************************/
+        /*** After Assertions ***/
+        /************************/
+
+        ReserveConfig[] memory configsAfter = createConfigurationSnapshot('', POOL);
+
+        _validateDaiInterestRateStrategy(
+            _findReserveConfigBySymbol(configsAfter, 'DAI').interestRateStrategy,
+            DAI_INTEREST_RATE_STRATEGY_NEW,
+            DaiInterestStrategyValues({
+                vat:                MCD_VAT,
+                pot:                MCD_POT,
+                ilk:                SPARK_ILK,
+                baseRateConversion: 1e27,
+                borrowSpread:       0,
+                supplySpread:       0,
+                maxRate:            0.75e27,
+                performanceBonus:   0
+            })
+        );
+        assertEq(_findReserveConfigBySymbol(configsAfter, 'sDAI').isFrozen, false);
+        vm.prank(address(poolAddressesProvider));
+        assertEq(BaseAdminUpgradeabilityProxy(payable(address(pool))).implementation(), POOL_IMPLEMENTATION_NEW);
+    }
+
+}
---
+++ src/proposals/20230816/SparkGoerli_20230816.t.sol
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.10;
+
+import '../../SparkTestBase.sol';
+
+import { Ownable } from 'aave-v3-core/contracts/dependencies/openzeppelin/contracts/Ownable.sol';
+import { BaseAdminUpgradeabilityProxy } from 'aave-v3-core/contracts/dependencies/openzeppelin/upgradeability/BaseAdminUpgradeabilityProxy.sol';
+
+import { SparkGoerli_20230816 } from './SparkGoerli_20230816.sol';
+
+contract SparkGoerli_20230816Test is SparkGoerliTestBase {
+
+    address constant internal PAUSE_PROXY = 0x5DCdbD3cCF9B09EAAD03bc5f50fA2B3d3ACA0121;
+
+    address public constant MCD_VAT = 0xB966002DDAa2Baf48369f5015329750019736031;
+    address public constant MCD_JUG = 0xC90C99FE9B5d5207A03b9F28A6E8A19C0e558916;
+    address public constant MCD_POT = 0x50672F0a14B40051B65958818a7AcA3D54Bd81Af;
+
+    address public constant DAI_INTEREST_RATE_STRATEGY_OLD
+        = 0x7f44e1c1dE70059D7cc483378BEFeE2a030CE247;
+
+    address public constant DAI_INTEREST_RATE_STRATEGY_NEW
+        = 0x70659BcA22A2a8BB324A526a8BB919185d3ecEBC;
+
+    bytes32 public constant SPARK_ILK = "DIRECT-SPARK-DAI";
+
+    IPool public constant POOL = IPool(0x26ca51Af4506DE7a6f0785D20CD776081a05fF6d);
+
+    address public constant POOL_IMPLEMENTATION_OLD = 0xF1E57711Eb5F897b415de1aEFCB64d9BAe58D312;
+    address public constant POOL_IMPLEMENTATION_NEW = 0xe7EA57b22D5F496BF9Ca50a7830547b704Ecb91F;
+
+    constructor() {
+        id = '20230816';
+    }
+
+    function setUp() public {
+        vm.createSelectFork(getChain('goerli').rpcUrl, 9_500_235);
+        payload = 0x13176Ad78eC3d2b6E32908B019D0F772EC0b4dFd;
+
+        // This will be done in the main spell (simulate it here)
+        loadPoolContext(poolAddressesProviderRegistry.getAddressesProvidersList()[0]);
+        vm.prank(PAUSE_PROXY);
+        Ownable(address(poolAddressesProvider)).transferOwnership(address(executor));
+    }
+
+    function testSpellSpecifics() public {
+
+        /*************************/
+        /*** Before Assertions ***/
+        /*************************/
+        
+        ReserveConfig[] memory configsBefore = createConfigurationSnapshot('', POOL);
+
+        _validateDaiJugInterestRateStrategy(
+            _findReserveConfigBySymbol(configsBefore, 'DAI').interestRateStrategy,
+            DAI_INTEREST_RATE_STRATEGY_OLD,
+            DaiJugInterestStrategyValues({
+                vat:                MCD_VAT,
+                jug:                MCD_JUG,
+                ilk:                SPARK_ILK,
+                baseRateConversion: 1e27,
+                borrowSpread:       0,
+                supplySpread:       0,
+                maxRate:            0.75e27,
+                performanceBonus:   0
+            })
+        );
+        assertEq(_findReserveConfigBySymbol(configsBefore, 'sDAI').isFrozen, true);
+        vm.prank(address(poolAddressesProvider));
+        assertEq(BaseAdminUpgradeabilityProxy(payable(address(pool))).implementation(), POOL_IMPLEMENTATION_OLD);
+
+        /***********************/
+        /*** Execute Payload ***/
+        /***********************/
+
+        GovHelpers.executePayload(vm, payload, executor);
+
+        /************************/
+        /*** After Assertions ***/
+        /************************/
+
+        ReserveConfig[] memory configsAfter = createConfigurationSnapshot('', POOL);
+
+        _validateDaiInterestRateStrategy(
+            _findReserveConfigBySymbol(configsAfter, 'DAI').interestRateStrategy,
+            DAI_INTEREST_RATE_STRATEGY_NEW,
+            DaiInterestStrategyValues({
+                vat:                MCD_VAT,
+                pot:                MCD_POT,
+                ilk:                SPARK_ILK,
+                baseRateConversion: 1e27,
+                borrowSpread:       0,
+                supplySpread:       0,
+                maxRate:            0.75e27,
+                performanceBonus:   0
+            })
+        );
+        assertEq(_findReserveConfigBySymbol(configsAfter, 'sDAI').isFrozen, false);
+        vm.prank(address(poolAddressesProvider));
+        assertEq(BaseAdminUpgradeabilityProxy(payable(address(pool))).implementation(), POOL_IMPLEMENTATION_NEW);
+    }
+
+}
