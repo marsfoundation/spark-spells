714ceb11d0acb20b1f42b4d5b9129a359cc45f6a
Bartek Rutkowski <barrutko.eth@gmail.com>
2023-09-04 11:14:26 +0200

    [SC-89] August 30 spell (#14)
    * feat: setup spells; change WETH market kink params
    
    * chore: fix alignment
    
    * test: add failing test for wstETH cap change
    
    * feat: add wstETH cap update to goerli spell
    
    * feat: add wstETH cap change to mainnet spell
    
    * chore: cleanup
    
    * refactor: extract constants in spells
    
    * refactor: minor aesthetics
    
    * chore: cleanup redundant reports
    
    * test: include testnet spell execution
    
    * refactor: use context; delete irrelevant setup
    
    * fix: correct variableRateSlope1 value
    
    * feat: add variableRateSlope2 update
    
    * docs: add forum links; update description
    
    * chore: aesthetic refactorings
    
    * chore: archive 20230816 spell
    
    * refactor: rename some variables in tests
    
    * fix: fix casting
    
    * docs: add votes links
    
    * fix: fix alignment
    
    * chore: remove unused imports
    
    * refactor: restric function state mutability
    
    * feat: remove block pin
    
    * refactor: add minor refactorings
    
    * fix: fix
    
    * chore: add new lines at file ends
    
    * feat: deploy spells
    
    * chore: merge master into branch
    
    * fix: add missing import
    
    * fix: add missing import

---
+++ src/proposals/20230830/SparkEthereum_20230830.t.sol
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.10;
+
+import '../../SparkTestBase.sol';
+
+import { SparkEthereum_20230830 } from './SparkEthereum_20230830.sol';
+
+contract SparkEthereum_20230830Test is SparkEthereumTestBase {
+
+    uint256 public constant OLD_WETH_OPTIMAL_USAGE_RATIO   = 0.80e27;
+    uint256 public constant NEW_WETH_OPTIMAL_USAGE_RATIO   = 0.90e27;
+    uint256 public constant OLD_WETH_VARIABLE_RATE_SLOPE_1 = 0.030e27;
+    uint256 public constant NEW_WETH_VARIABLE_RATE_SLOPE_1 = 0.028e27;
+    uint256 public constant OLD_WETH_VARIABLE_RATE_SLOPE_2 = 0.80e27;
+    uint256 public constant NEW_WETH_VARIABLE_RATE_SLOPE_2 = 1.20e27;
+    uint256 public constant OLD_WSTETH_SUPPLY_CAP          = 200_000;
+    uint256 public constant NEW_WSTETH_SUPPLY_CAP          = 400_000;
+
+    constructor() {
+        id = '20230830';
+    }
+
+    function setUp() public {
+        vm.createSelectFork(getChain('mainnet').rpcUrl, 17_970_910);
+        payload = 0xFBdB6C5596Fc958B432Bf1c99268C72B1515DFf0;
+
+        loadPoolContext(poolAddressesProviderRegistry.getAddressesProvidersList()[0]);
+    }
+
+    function testSpellSpecifics() public {
+
+        ReserveConfig[] memory allConfigsBefore = createConfigurationSnapshot('', pool);
+
+        /*******************************************/
+        /*** wstETH Supply Cap Before Assertions ***/
+        /*******************************************/
+
+        ReserveConfig memory wstETHConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'wstETH');
+        assertEq(wstETHConfigBefore.supplyCap, OLD_WSTETH_SUPPLY_CAP);
+
+        /*****************************************************/
+        /*** WETH Interest Rate Strategy Before Assertions ***/
+        /*****************************************************/
+
+        ReserveConfig memory wethConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'WETH');
+        IDefaultInterestRateStrategy interestRateStrategy = IDefaultInterestRateStrategy(
+            wethConfigBefore.interestRateStrategy
+        );
+
+        _validateInterestRateStrategy(
+            address(interestRateStrategy),
+            address(interestRateStrategy),
+            InterestStrategyValues({
+                addressesProvider:             address(poolAddressesProvider),
+                optimalUsageRatio:             OLD_WETH_OPTIMAL_USAGE_RATIO,
+                optimalStableToTotalDebtRatio: interestRateStrategy.OPTIMAL_STABLE_TO_TOTAL_DEBT_RATIO(),
+                baseStableBorrowRate:          OLD_WETH_VARIABLE_RATE_SLOPE_1,
+                stableRateSlope1:              interestRateStrategy.getStableRateSlope1(),
+                stableRateSlope2:              interestRateStrategy.getStableRateSlope2(),
+                baseVariableBorrowRate:        interestRateStrategy.getBaseVariableBorrowRate(),
+                variableRateSlope1:            OLD_WETH_VARIABLE_RATE_SLOPE_1,
+                variableRateSlope2:            OLD_WETH_VARIABLE_RATE_SLOPE_2
+            })
+        );
+
+        /***********************/
+        /*** Execute Payload ***/
+        /***********************/
+
+        GovHelpers.executePayload(vm, payload, executor);
+
+        ReserveConfig[] memory allConfigsAfter = createConfigurationSnapshot('', pool);
+
+        /******************************************/
+        /*** wstETH Supply Cap After Assertions ***/
+        /******************************************/
+
+        wstETHConfigBefore.supplyCap = NEW_WSTETH_SUPPLY_CAP;
+        _validateReserveConfig(wstETHConfigBefore, allConfigsAfter);
+
+        /****************************************************/
+        /*** WETH Interest Rate Strategy After Assertions ***/
+        /****************************************************/
+
+        ReserveConfig memory wethConfigAfter = _findReserveConfigBySymbol(allConfigsAfter, 'WETH');
+
+        _validateInterestRateStrategy(
+            wethConfigAfter.interestRateStrategy,
+            wethConfigAfter.interestRateStrategy,
+            InterestStrategyValues({
+                addressesProvider:             address(poolAddressesProvider),
+                optimalUsageRatio:             NEW_WETH_OPTIMAL_USAGE_RATIO,
+                optimalStableToTotalDebtRatio: interestRateStrategy.OPTIMAL_STABLE_TO_TOTAL_DEBT_RATIO(),
+                baseStableBorrowRate:          NEW_WETH_VARIABLE_RATE_SLOPE_1,
+                stableRateSlope1:              interestRateStrategy.getStableRateSlope1(),
+                stableRateSlope2:              interestRateStrategy.getStableRateSlope2(),
+                baseVariableBorrowRate:        interestRateStrategy.getBaseVariableBorrowRate(),
+                variableRateSlope1:            NEW_WETH_VARIABLE_RATE_SLOPE_1,
+                variableRateSlope2:            NEW_WETH_VARIABLE_RATE_SLOPE_2
+            })
+        );
+    }
+}
---
+++ src/proposals/20230830/SparkGoerli_20230830.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.10;
+
+import { SparkPayloadGoerli, IEngine, Rates, EngineFlags } from '../../SparkPayloadGoerli.sol';
+
+/**
+ * @title  August 30, 2023 Spark Goerli Proposal - Update ETH utilization rate parameters, increase wstETH supply cap
+ * @author Phoenix Labs
+ * @dev    This proposal updates ETH market optimalUsageRatio, variableRateSlope1, variableRateSlope2 parameters and raises wstETH supplyCap
+ * Forum:       https://forum.makerdao.com/t/phoenix-labs-proposed-changes-for-spark-for-next-upcoming-spell/21685
+ * ETH Vote:    https://vote.makerdao.com/polling/QmULJKwK
+ * wstETH Vote: https://vote.makerdao.com/polling/QmbMR8PU
+ */
+contract SparkGoerli_20230830 is SparkPayloadGoerli {
+
+    address public constant WETH   = 0x7D5afF7ab67b431cDFA6A94d50d3124cC4AB2611;
+    address public constant WSTETH = 0x6E4F1e8d4c5E5E6e2781FD814EE0744cc16Eb352;
+
+    uint256 public constant NEW_WETH_OPTIMAL_USAGE_RATIO   = 0.90e27;
+    uint256 public constant NEW_WETH_VARIABLE_RATE_SLOPE_1 = 0.028e27;
+    uint256 public constant NEW_WETH_VARIABLE_RATE_SLOPE_2 = 1.20e27;
+    uint256 public constant NEW_WSTETH_SUPPLY_CAP          = 400_000;
+
+    function rateStrategiesUpdates()
+        public view override returns (IEngine.RateStrategyUpdate[] memory)
+    {
+        IEngine.RateStrategyUpdate[] memory ratesUpdate = new IEngine.RateStrategyUpdate[](1);
+
+        Rates.RateStrategyParams memory weth = LISTING_ENGINE
+            .RATE_STRATEGIES_FACTORY()
+            .getStrategyDataOfAsset(WETH);
+
+        weth.optimalUsageRatio  = NEW_WETH_OPTIMAL_USAGE_RATIO;
+        weth.variableRateSlope1 = NEW_WETH_VARIABLE_RATE_SLOPE_1;
+        weth.variableRateSlope2 = NEW_WETH_VARIABLE_RATE_SLOPE_2;
+
+        ratesUpdate[0] = IEngine.RateStrategyUpdate({
+            asset:  WETH,
+            params: weth
+        });
+
+        return ratesUpdate;
+    }
+
+    function capsUpdates()
+        public pure override returns (IEngine.CapsUpdate[] memory)
+    {
+        IEngine.CapsUpdate[] memory capsUpdate = new IEngine.CapsUpdate[](1);
+        capsUpdate[0] = IEngine.CapsUpdate({
+            asset:     WSTETH,
+            supplyCap: NEW_WSTETH_SUPPLY_CAP,
+            borrowCap: EngineFlags.KEEP_CURRENT
+        });
+
+        return capsUpdate;
+    }
+}
