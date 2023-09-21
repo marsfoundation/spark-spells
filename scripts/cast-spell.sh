02fbb93654e7b0ba29c253570eae66743b842b60
TomekSetner <tomek.setner@gmail.com>
2023-09-21 08:46:30 +0200

    FRO-40: set user address

--- scripts/cast-spell.sh
+++ scripts/cast-spell.sh
@@ -6,8 +6,10 @@ spell_addr=$2
 spell_executor=$3
 rpc_url=$4
 
+user_address=$(cast wallet address --private-key $anvil_priv_key)
+
 echo "Replacing code of spell (${spell_addr:0:7}) with executor (${spell_executor:0:7})..."
 code=$(cast rpc eth_getCode $spell_addr latest --rpc-url $rpc_url)
 cast rpc anvil_setCode $spell_executor $code --rpc-url $rpc_url
-cast send --private-key $anvil_priv_key $spell_executor --rpc-url $rpc_url "execute()"
+cast send --private-key $anvil_priv_key $spell_executor --rpc-url $rpc_url --from $user_address "execute()"
 
--- scripts/fork-and-cast-spell.sh
+++ scripts/fork-and-cast-spell.sh
@@ -11,6 +11,7 @@ port=${5-8545}
 anvil_priv_key=${6-0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80}
 
 rpc_url=http://localhost:$port
+user_address=$(cast wallet address --private-key $anvil_priv_key)
 
 if [[ -n $anvil_fork_block_number ]]; then
     extra_params="--fork-block-number $anvil_fork_block_number"
@@ -26,7 +27,7 @@ if [[ $spell_path_or_address == 0x* && ${#spell_path_or_address} -eq 42 ]]; then
     spell_addr=$spell_path_or_address
 else
     forge build
-    spell_addr=$(forge create $spell_path_or_address --private-key $anvil_priv_key --rpc-url $rpc_url | grep "Deployed to:" | awk '{print $3}')
+    spell_addr=$(forge create $spell_path_or_address --private-key $anvil_priv_key --rpc-url $rpc_url --from $user_address | grep "Deployed to:" | awk '{print $3}')
 fi
 
 cd "$(dirname "$0")"
