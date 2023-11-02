df874b7fca197bb92a8362f3ccafb8ae6f115f81
Bartek Rutkowski <barrutko.eth@gmail.com>
2023-11-02 14:33:26 +0100

    feat: start working on SparkEthereum_20231115

---
+++ src/proposals/SparkEthereum_20231115.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.10;
+
+import { SparkPayloadEthereum, IEngine, EngineFlags } from '../SparkPayloadEthereum.sol';
+
+/**
+ * @title  November 15, 2023 Spark Ethereum Proposal -
+ * @author Phoenix Labs
+ * @dev
+ * Forum:
+ * Vote:
+ */
+contract SparkEthereum_20231115 is SparkPayloadEthereum {
+    address public constant RETH   = 0xae78736Cd615f374D3085123A210448E74Fc6393;
+    address public constant WSTETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
+    address public constant DAI    = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+
+    uint256 constant NEW_RETH_SUPPLY_CAP   = 80_000;
+    uint256 constant NEW_WSTETH_SUPPLY_CAP = 600_000;
+    uint256 constant NEW_DAI_LTV           = 0;
+
+    function capsUpdates()
+        public pure override returns (IEngine.CapsUpdate[] memory)
+    {
+        IEngine.CapsUpdate[] memory capsUpdate = new IEngine.CapsUpdate[](2);
+        capsUpdate[0] = IEngine.CapsUpdate({
+            asset:     RETH,
+            supplyCap: NEW_RETH_SUPPLY_CAP,
+            borrowCap: EngineFlags.KEEP_CURRENT
+        });
+
+        capsUpdate[1] = IEngine.CapsUpdate({
+            asset:     WSTETH,
+            supplyCap: NEW_WSTETH_SUPPLY_CAP,
+            borrowCap: EngineFlags.KEEP_CURRENT
+        });
+
+        return capsUpdate;
+    }
+
+    function collateralsUpdates()
+        public pure override returns (IEngine.CollateralUpdate[] memory)
+    {
+        IEngine.CollateralUpdate[] memory collateralUpdates = new IEngine.CollateralUpdate[](1);
+
+        collateralUpdates[0] = IEngine.CollateralUpdate({
+            asset:          DAI,
+            ltv:            0,
+            liqThreshold:   EngineFlags.KEEP_CURRENT,
+            liqBonus:       EngineFlags.KEEP_CURRENT,
+            debtCeiling:    EngineFlags.KEEP_CURRENT,
+            liqProtocolFee: EngineFlags.KEEP_CURRENT,
+            eModeCategory:  EngineFlags.KEEP_CURRENT
+        });
+
+        return collateralUpdates;
+    }
+}
---
+++ src/proposals/SparkEthereum_20231115.t.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.10;
+
+import '../SparkTestBase.sol';
+
+import { SparkEthereum_20231115 } from './SparkEthereum_20231115.sol';
+
+contract SparkEthereum_20231115Test is SparkEthereumTestBase {
+
+    uint256 public constant OLD_RETH_SUPPLY_CAP = 60_000;
+    uint256 public constant NEW_RETH_SUPPLY_CAP = 80_000;
+
+    uint256 public constant OLD_WSTETH_SUPPLY_CAP = 400_000;
+    uint256 public constant NEW_WSTETH_SUPPLY_CAP = 600_000; // TBD
+
+    uint256 public constant OLD_DAI_LTV = 1;
+    uint256 public constant NEW_DAI_LTV = 0;
+
+    constructor() {
+        id = '20231115';
+    }
+
+    function setUp() public {
+        vm.createSelectFork(getChain('mainnet').rpcUrl, 18484640);
+        payload = deployPayload();
+
+        loadPoolContext(poolAddressesProviderRegistry.getAddressesProvidersList()[0]);
+    }
+
+    function testSpellSpecifics() public {
+
+        ReserveConfig[] memory allConfigsBefore = createConfigurationSnapshot('', pool);
+
+        /*****************************************/
+        /*** rETH Supply Cap Before Assertions ***/
+        /*******************************************/
+
+        ReserveConfig memory rETHConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'rETH');
+        assertEq(rETHConfigBefore.supplyCap, OLD_RETH_SUPPLY_CAP);
+
+        /*******************************************/
+        /*** wstETH Supply Cap Before Assertions ***/
+        /*******************************************/
+
+        ReserveConfig memory wstETHConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'wstETH');
+        assertEq(wstETHConfigBefore.supplyCap, OLD_WSTETH_SUPPLY_CAP);
+
+        /*********************************/
+        /*** DAI LTV Before Assertions ***/
+        /*********************************/
+
+        ReserveConfig memory DAIConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'DAI');
+        assertEq(DAIConfigBefore.ltv, OLD_DAI_LTV);
+
+        /***********************/
+        /*** Execute Payload ***/
+        /***********************/
+
+        GovHelpers.executePayload(vm, payload, executor);
+
+        ReserveConfig[] memory allConfigsAfter = createConfigurationSnapshot('', pool);
+
+        /******************************************/
+        /*** rETH Supply Cap After Assertions ***/
+        /******************************************/
+
+        rETHConfigBefore.supplyCap = NEW_RETH_SUPPLY_CAP;
+        _validateReserveConfig(rETHConfigBefore, allConfigsAfter);
+
+        /******************************************/
+        /*** wstETH Supply Cap After Assertions ***/
+        /******************************************/
+
+        wstETHConfigBefore.supplyCap = NEW_WSTETH_SUPPLY_CAP;
+        _validateReserveConfig(wstETHConfigBefore, allConfigsAfter);
+
+        /********************************/
+        /*** DAI LTV After Assertions ***/
+        /********************************/
+
+        DAIConfigBefore.ltv = 0;
+        _validateReserveConfig(DAIConfigBefore, allConfigsAfter);
+    }
+
+}
