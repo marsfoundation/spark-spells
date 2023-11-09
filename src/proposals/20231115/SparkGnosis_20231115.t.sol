ce071cfa9bd9986fb1ea5189603d76be87407c70
Bartek Rutkowski <barrutko.eth@gmail.com>
2023-11-09 11:25:22 +0100

    refactor: readd testSpellSpecifics to gnosis tests

--- src/proposals/20231115/SparkGnosis_20231115.t.sol
+++ src/proposals/20231115/SparkGnosis_20231115.t.sol
@@ -12,10 +12,84 @@ contract SparkGnosis_20231115Test is SparkGnosisTestBase {
     }
 
     function setUp() public {
-        vm.createSelectFork(getChain('gnosis_chain').rpcUrl, 30_855_380);
+        vm.createSelectFork(getChain('gnosis_chain').rpcUrl, 30_855_038);
         payload = 0x41709f51E59ddbEbF37cE95257b2E4f2884a45F8;
 
         loadPoolContext(poolAddressesProviderRegistry.getAddressesProvidersList()[0]);
+    }
+
+    function testSpellSpecifics() public {
+
+        ReserveConfig[] memory allConfigsBefore = createConfigurationSnapshot('', pool);
+
+        /*******************************************/
+        /*** wstETH Supply Cap Before Assertions ***/
+        /*******************************************/
+
+        ReserveConfig memory wstETHConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'wstETH');
+        assertEq(wstETHConfigBefore.supplyCap, 5_000);
+
+        /*****************************************************/
+        /*** WETH Interest Rate Strategy Before Assertions ***/
+        /*****************************************************/
+
+        ReserveConfig memory wethConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'WETH');
+        IDefaultInterestRateStrategy oldInterestRateStrategy = IDefaultInterestRateStrategy(
+            wethConfigBefore.interestRateStrategy
+        );
+
+        _validateInterestRateStrategy(
+            address(oldInterestRateStrategy),
+            address(oldInterestRateStrategy),
+            InterestStrategyValues({
+                addressesProvider:             address(poolAddressesProvider),
+                optimalUsageRatio:             oldInterestRateStrategy.OPTIMAL_USAGE_RATIO(),
+                optimalStableToTotalDebtRatio: oldInterestRateStrategy.OPTIMAL_STABLE_TO_TOTAL_DEBT_RATIO(),
+                baseStableBorrowRate:          0.028e27,
+                stableRateSlope1:              oldInterestRateStrategy.getStableRateSlope1(),
+                stableRateSlope2:              oldInterestRateStrategy.getStableRateSlope2(),
+                baseVariableBorrowRate:        0.01e27,
+                variableRateSlope1:            0.028e27,
+                variableRateSlope2:            oldInterestRateStrategy.getVariableRateSlope2()
+            })
+        );
+
+        /***********************/
+        /*** Execute Payload ***/
+        /***********************/
+
+        GovHelpers.executePayload(vm, payload, executor);
+
+        ReserveConfig[] memory allConfigsAfter = createConfigurationSnapshot('', pool);
+
+        /******************************************/
+        /*** wstETH Supply Cap After Assertions ***/
+        /******************************************/
+
+        wstETHConfigBefore.supplyCap = 10_000;
+        _validateReserveConfig(wstETHConfigBefore, allConfigsAfter);
+
+        /****************************************************/
+        /*** WETH Interest Rate Strategy After Assertions ***/
+        /****************************************************/
+
+        ReserveConfig memory wethConfigAfter = _findReserveConfigBySymbol(allConfigsAfter, 'WETH');
+
+        _validateInterestRateStrategy(
+            wethConfigAfter.interestRateStrategy,
+            wethConfigAfter.interestRateStrategy,
+            InterestStrategyValues({
+                addressesProvider:             address(poolAddressesProvider),
+                optimalUsageRatio:             oldInterestRateStrategy.OPTIMAL_USAGE_RATIO(),
+                optimalStableToTotalDebtRatio: oldInterestRateStrategy.OPTIMAL_STABLE_TO_TOTAL_DEBT_RATIO(),
+                baseStableBorrowRate:          0.032e27,
+                stableRateSlope1:              oldInterestRateStrategy.getStableRateSlope1(),
+                stableRateSlope2:              oldInterestRateStrategy.getStableRateSlope2(),
+                baseVariableBorrowRate:        0,
+                variableRateSlope1:            0.032e27,
+                variableRateSlope2:            oldInterestRateStrategy.getVariableRateSlope2()
+            })
+        );
     }
 
 }
