12bf976c763f813a97475e7010ed52e1e5886faf
lucas-manuel <lucasmanuel.tech@gmail.com>
2023-12-01 12:04:40 -0500

    fix: warp longer to fix low interest rate market e2e test

---
+++ src/proposals/SparkE2ETest.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.10;
+
+import '../SparkTestBase.sol';
+
+contract SparkEthereum_E2ETest is SparkEthereumTestBase {
+
+    constructor() {
+        id = '20231129';
+    }
+
+    function setUp() public {
+        vm.createSelectFork(getChain('mainnet').rpcUrl, 18_692_754);
+        payload = 0x68a075249fA77173b8d1B92750c9920423997e2B;
+
+        loadPoolContext(poolAddressesProviderRegistry.getAddressesProvidersList()[0]);
+    }
+
+}
