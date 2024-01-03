8abb68da065111108d95e470f53b33d8719e8455
Sam MacPherson <xylom44@gmail.com>
2024-01-03 10:51:05 -0500

    started on spell

---
+++ src/proposals/20240110/SparkEthereum_20240110.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.10;
+
+import { SparkPayloadEthereum, IEngine, EngineFlags } from '../../SparkPayloadEthereum.sol';
+
+import { IACLManager } from 'aave-v3-core/contracts/interfaces/IACLManager.sol';
+
+import { IEmissionManager }   from "aave-v3-periphery/rewards/interfaces/IEmissionManager.sol";
+import { IRewardsController } from "aave-v3-periphery/rewards/interfaces/IRewardsController.sol";
+
+import { ISparkLendFreezerMom } from './ISparkLendFreezerMom.sol';
+
+/**
+ * @title  January 10, 2024 Spark Ethereum Proposal
+ * @author Phoenix Labs
+ * @dev    Activate Freezer Mom, DAI oracle to hardcoded $1, wstETH oracle assume 1:1 stETH peg, Freeze GNO, Activate Lido Rewards.
+ * Forum:  https://forum.makerdao.com/t/spark-spell-proposed-changes/23298
+ * Polls:  TODO
+ */
+contract SparkEthereum_20240110 is SparkPayloadEthereum {
+
+    address constant FREEZER_MOM        = ;  // TODO deploy
+    address constant AUTHORITY          = 0x0a3f6849f78076aefaDf113F5BED87720274dDC0;
+    address constant ACL_MANAGER        = 0xdA135Cd78A086025BcdC87B038a1C462032b510C;
+    address constant DAI                = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address constant DAI_ORACLE         = ;  // TODO deploy
+    address constant WSTETH             = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
+    address constant WSTETH_ORACLE      = 0x8B6851156023f4f5A66F68BEA80851c3D905Ac93;
+    address constant GNO                = 0x6810e776880C02933D47DB1b9fc05908e5386b96;
+    address constant TRANSFER_STRATEGY  = ;  // TODO deploy
+    address constant EMISSION_MANAGER   = 0xf09e48dd4CA8e76F63a57ADd428bB06fee7932a4;
+    address constant REWARDS_CONTROLLER = 0x4370D3b6C9588E02ce9D22e684387859c7Ff5b34;
+    address constant REWARDS_OPERATOR   = 0x8076807464DaC94Ac8Aa1f7aF31b58F73bD88A27;  // Operator multi-sig (also custodies the rewards)
+
+    function priceFeedsUpdates() public view override returns (IEngine.PriceFeedUpdate[] memory) {
+        IEngine.PriceFeedUpdate[] memory updates = new IEngine.PriceFeedUpdate[](2);
+
+        updates[0] = IEngine.PriceFeedUpdate({
+            asset:     DAI,
+            priceFeed: DAI_ORACLE
+        });
+        updates[1] = IEngine.PriceFeedUpdate({
+            asset:     WSTETH,
+            priceFeed: WSTETH_ORACLE
+        });
+
+        return updates;
+    }
+
+    function collateralsUpdates() public view override returns (IEngine.CollateralUpdate[] memory) {
+        IEngine.CollateralUpdate[] memory updates = new IEngine.CollateralUpdate[](1);
+
+        updates[0] = IEngine.CollateralUpdate({
+            asset:          GNO,
+            ltv:            0,
+            liqThreshold:   EngineFlags.KEEP_CURRENT,
+            liqBonus:       EngineFlags.KEEP_CURRENT,
+            debtCeiling:    EngineFlags.KEEP_CURRENT,
+            liqProtocolFee: EngineFlags.KEEP_CURRENT,
+            eModeCategory:  EngineFlags.KEEP_CURRENT
+        });
+
+        return updates;
+    }
+
+    function _postExecute() internal override {
+        // --- Activate the Freezer Mom ---
+        ISparkLendFreezerMom freezerMom = ISparkLendFreezerMom(FREEZER_MOM);
+
+        require(freezerMom.pool()             == LISTING_ENGINE.POOL(),              "pool mismatch");
+        require(freezerMom.poolConfigurator() == LISTING_ENGINE.POOL_CONFIGURATOR(), "poolConfigurator mismatch");
+
+        freezerMom.setAuthority(AUTHORITY);
+        IACLManager(ACL_MANAGER).addEmergencyAdmin(address(freezerMom));
+        IACLManager(ACL_MANAGER).addRiskAdmin(address(freezerMom));
+
+        // --- Activate Lido Rewards ---
+        emissionManager.setEmissionAdmin(WSTETH, REWARDS_OPERATOR);
+    }
+
+}
---
+++ src/proposals/20240110/SparkEthereum_20240110.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.10;
+
+import '../../SparkTestBase.sol';
+
+contract SparkEthereum_20240110Test is SparkEthereumTestBase {
+
+    constructor() {
+        id = '20240110';
+    }
+
+    function setUp() public {
+        vm.createSelectFork(getChain('mainnet').rpcUrl);
+        payload = deployPayload();
+
+        loadPoolContext(poolAddressesProviderRegistry.getAddressesProvidersList()[0]);
+    }
+
+    function testSpellSpecifics() public {
+    }
+
+}
