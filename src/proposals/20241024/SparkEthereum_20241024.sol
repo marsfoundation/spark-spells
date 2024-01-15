bd4ca45dfba3c58fbcab37be5bfa8c7e723b6b97
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-01-15 18:53:43 +0100

    feat: add placeholder IRM update

--- src/proposals/20241024/SparkEthereum_20241024.sol
+++ src/proposals/20241024/SparkEthereum_20241024.sol
@@ -14,13 +14,19 @@ contract SparkEthereum_20241024 is SparkPayloadEthereum {
 
     using Address for address;
 
-    address public constant FIXED_PRICE_ORACLE = 0x42a03F81dd8A1cEcD746dc262e4d1CD9fD39F777;
+    address public constant DAI                = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address public constant USDC               = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address public constant USDT               = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+    address public constant DAI_IRM            = 0x7d8f2210FAD012E7d260C3ddBeCaCfd48277455F;  // TBA (these are placeholders)
+    address public constant USDC_IRM           = 0xbc8A68B0ab0617D7c90d15bb1601B25d795Dc4c8;  // TBA (these are placeholders)
+    address public constant USDT_IRM           = 0xbc8A68B0ab0617D7c90d15bb1601B25d795Dc4c8;  // TBA (these are placeholders)
     address public constant WBTC               = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
+    address public constant FIXED_PRICE_ORACLE = 0x42a03F81dd8A1cEcD746dc262e4d1CD9fD39F777;
 
 
-    function priceFeedsUpdates() public pure override returns (IEngine.PriceFeedUpdate[] memory) {
+    function priceFeedsUpdates()
+        public pure override returns (IEngine.PriceFeedUpdate[] memory)
+    {
         IEngine.PriceFeedUpdate[] memory updates = new IEngine.PriceFeedUpdate[](2);
 
         updates[0] = IEngine.PriceFeedUpdate({
@@ -48,4 +54,23 @@ contract SparkEthereum_20241024 is SparkPayloadEthereum {
         return capsUpdate;
     }
 
+    function _postExecute()
+        internal override
+    {
+        LISTING_ENGINE.POOL_CONFIGURATOR().setReserveInterestRateStrategyAddress(
+            DAI,
+            DAI_IRM
+        );
+
+        LISTING_ENGINE.POOL_CONFIGURATOR().setReserveInterestRateStrategyAddress(
+            USDC,
+            USDC_IRM
+        );
+
+        LISTING_ENGINE.POOL_CONFIGURATOR().setReserveInterestRateStrategyAddress(
+            USDT,
+            USDT_IRM
+        );
+
+    }
 }
--- src/proposals/20241024/SparkEthereum_20241024.t.sol
+++ src/proposals/20241024/SparkEthereum_20241024.t.sol
@@ -7,11 +7,19 @@ import { SparkEthereum_20241024 } from './SparkEthereum_20241024.sol';
 
 contract SparkEthereum_20241024Test is SparkEthereumTestBase {
 
-    address public constant USDC              = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    address public constant OLD_USDC_ORACLE   = 0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6;
-    address public constant USDT              = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
-    address public constant OLD_USDT_ORACLE   = 0x3E7d1eAB13ad0104d2750B8863b489D65364e32D;
-    address public constant FIXEDPRICE_ORACLE = 0x42a03F81dd8A1cEcD746dc262e4d1CD9fD39F777;
+    address public constant USDC                            = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public constant USDT                            = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+
+    address public constant OLD_USDC_ORACLE                 = 0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6;
+    address public constant OLD_USDT_ORACLE                 = 0x3E7d1eAB13ad0104d2750B8863b489D65364e32D;
+    address public constant FIXED_PRICE_ORACLE              = 0x42a03F81dd8A1cEcD746dc262e4d1CD9fD39F777;
+
+    address public constant OLD_DAI_INTEREST_RATE_STRATEGY  = 0x7d8f2210FAD012E7d260C3ddBeCaCfd48277455F;
+    address public constant NEW_DAI_INTEREST_RATE_STRATEGY  = 0x7d8f2210FAD012E7d260C3ddBeCaCfd48277455F; // TBA (these are placeholders)
+    address public constant OLD_USDC_INTEREST_RATE_STRATEGY = 0xbc8A68B0ab0617D7c90d15bb1601B25d795Dc4c8;
+    address public constant NEW_USDC_INTEREST_RATE_STRATEGY = 0xbc8A68B0ab0617D7c90d15bb1601B25d795Dc4c8; // TBA (these are placeholders)
+    address public constant OLD_USDT_INTEREST_RATE_STRATEGY = 0xbc8A68B0ab0617D7c90d15bb1601B25d795Dc4c8;
+    address public constant NEW_USDT_INTEREST_RATE_STRATEGY = 0xbc8A68B0ab0617D7c90d15bb1601B25d795Dc4c8; // TBA (these are placeholders)
 
     uint256 public constant OLD_WBTC_SUPPLY_CAP = 3_000;
     uint256 public constant NEW_WBTC_SUPPLY_CAP = 5_000;
@@ -42,8 +50,21 @@ contract SparkEthereum_20241024Test is SparkEthereumTestBase {
         /*** WBTC Supply Cap Before Assertions ***/
         /*****************************************/
 
-        ReserveConfig memory WBTCConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'WBTC');
-        assertEq(WBTCConfigBefore.supplyCap, OLD_WBTC_SUPPLY_CAP);
+        ReserveConfig memory wbtcConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'WBTC');
+        assertEq(wbtcConfigBefore.supplyCap, OLD_WBTC_SUPPLY_CAP);
+
+        /*************************************************/
+        /*** DAI, USDC & USDT Oracle Before Assertions ***/
+        /*************************************************/
+
+        ReserveConfig memory daiConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'DAI');
+        assertEq(daiConfigBefore.interestRateStrategy, OLD_DAI_INTEREST_RATE_STRATEGY);
+
+        ReserveConfig memory usdcConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'USDC');
+        assertEq(usdcConfigBefore.interestRateStrategy, OLD_USDC_INTEREST_RATE_STRATEGY);
+
+        ReserveConfig memory usdtConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'USDT');
+        assertEq(usdtConfigBefore.interestRateStrategy, OLD_USDT_INTEREST_RATE_STRATEGY);
 
         /***********************/
         /*** Execute Payload ***/
@@ -57,15 +78,28 @@ contract SparkEthereum_20241024Test is SparkEthereumTestBase {
         /*** USDC & USDT Oracle After Assertions ***/
         /*******************************************/
 
-        _validateAssetSourceOnOracle(poolAddressesProvider, USDC, FIXEDPRICE_ORACLE);
-        _validateAssetSourceOnOracle(poolAddressesProvider, USDT, FIXEDPRICE_ORACLE);
+        _validateAssetSourceOnOracle(poolAddressesProvider, USDC, FIXED_PRICE_ORACLE);
+        _validateAssetSourceOnOracle(poolAddressesProvider, USDT, FIXED_PRICE_ORACLE);
 
         /****************************************/
         /*** WBTC Supply Cap After Assertions ***/
         /****************************************/
 
-        WBTCConfigBefore.supplyCap = NEW_WBTC_SUPPLY_CAP;
-        _validateReserveConfig(WBTCConfigBefore, allConfigsAfter);
+        wbtcConfigBefore.supplyCap = NEW_WBTC_SUPPLY_CAP;
+        _validateReserveConfig(wbtcConfigBefore, allConfigsAfter);
+
+        /************************************************/
+        /*** DAI, USDC & USDT Oracle After Assertions ***/
+        /************************************************/
+
+        ReserveConfig memory daiConfigAfter = _findReserveConfigBySymbol(allConfigsAfter, 'DAI');
+        assertEq(daiConfigAfter.interestRateStrategy, NEW_DAI_INTEREST_RATE_STRATEGY);
+
+        ReserveConfig memory usdcConfigAfter = _findReserveConfigBySymbol(allConfigsAfter, 'USDC');
+        assertEq(usdcConfigAfter.interestRateStrategy, NEW_USDC_INTEREST_RATE_STRATEGY);
+
+        ReserveConfig memory usdtConfigAfter = _findReserveConfigBySymbol(allConfigsAfter, 'USDT');
+        assertEq(usdtConfigAfter.interestRateStrategy, NEW_USDT_INTEREST_RATE_STRATEGY);
     }
 
 }
