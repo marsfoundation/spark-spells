ed240c6ccb44a314eda19c78569fadfbaee52c18
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-01-15 14:55:01 +0100

    feat: update USDC and USDT oracle

--- src/proposals/20241024/SparkEthereum_20241024.sol
+++ src/proposals/20241024/SparkEthereum_20241024.sol
@@ -14,7 +14,26 @@ contract SparkEthereum_20241024 is SparkPayloadEthereum {
 
     using Address for address;
 
-    address public constant WBTC = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
+    address public constant FIXED_PRICE_ORACLE = 0x42a03F81dd8A1cEcD746dc262e4d1CD9fD39F777;
+    address public constant USDC               = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public constant USDT               = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+    address public constant WBTC               = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
+
+
+    function priceFeedsUpdates() public pure override returns (IEngine.PriceFeedUpdate[] memory) {
+        IEngine.PriceFeedUpdate[] memory updates = new IEngine.PriceFeedUpdate[](2);
+
+        updates[0] = IEngine.PriceFeedUpdate({
+            asset:     USDC,
+            priceFeed: FIXED_PRICE_ORACLE
+        });
+        updates[1] = IEngine.PriceFeedUpdate({
+            asset:     USDT,
+            priceFeed: FIXED_PRICE_ORACLE
+        });
+
+        return updates;
+    }
 
     function capsUpdates()
         public pure override returns (IEngine.CapsUpdate[] memory)
--- src/proposals/20241024/SparkEthereum_20241024.t.sol
+++ src/proposals/20241024/SparkEthereum_20241024.t.sol
@@ -7,6 +7,15 @@ import { SparkEthereum_20241024 } from './SparkEthereum_20241024.sol';
 
 contract SparkEthereum_20241024Test is SparkEthereumTestBase {
 
+    address public constant USDC              = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public constant OLD_USDC_ORACLE   = 0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6;
+    address public constant USDT              = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+    address public constant OLD_USDT_ORACLE   = 0x3E7d1eAB13ad0104d2750B8863b489D65364e32D;
+    address public constant FIXEDPRICE_ORACLE = 0x42a03F81dd8A1cEcD746dc262e4d1CD9fD39F777;
+
+    uint256 public constant OLD_WBTC_SUPPLY_CAP = 3_000;
+    uint256 public constant NEW_WBTC_SUPPLY_CAP = 5_000;
+
     constructor() {
         id = '20241024';
     }
@@ -22,12 +31,19 @@ contract SparkEthereum_20241024Test is SparkEthereumTestBase {
 
         ReserveConfig[] memory allConfigsBefore = createConfigurationSnapshot('', pool);
 
+        /********************************************/
+        /*** USDC & USDT Oracle Before Assertions ***/
+        /********************************************/
+
+        _validateAssetSourceOnOracle(poolAddressesProvider, USDC, OLD_USDC_ORACLE);
+        _validateAssetSourceOnOracle(poolAddressesProvider, USDT, OLD_USDT_ORACLE);
+
         /*****************************************/
         /*** WBTC Supply Cap Before Assertions ***/
-        /*******************************************/
+        /*****************************************/
 
         ReserveConfig memory WBTCConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'WBTC');
-        assertEq(WBTCConfigBefore.supplyCap, 3_000);
+        assertEq(WBTCConfigBefore.supplyCap, OLD_WBTC_SUPPLY_CAP);
 
         /***********************/
         /*** Execute Payload ***/
@@ -37,11 +53,18 @@ contract SparkEthereum_20241024Test is SparkEthereumTestBase {
 
         ReserveConfig[] memory allConfigsAfter = createConfigurationSnapshot('', pool);
 
-        /******************************************/
-        /*** WBTC Supply Cap After Assertions ***/
-        /******************************************/
+        /*******************************************/
+        /*** USDC & USDT Oracle After Assertions ***/
+        /*******************************************/
 
-        WBTCConfigBefore.supplyCap = 5_000;
+        _validateAssetSourceOnOracle(poolAddressesProvider, USDC, FIXEDPRICE_ORACLE);
+        _validateAssetSourceOnOracle(poolAddressesProvider, USDT, FIXEDPRICE_ORACLE);
+
+        /****************************************/
+        /*** WBTC Supply Cap After Assertions ***/
+        /****************************************/
+
+        WBTCConfigBefore.supplyCap = NEW_WBTC_SUPPLY_CAP;
         _validateReserveConfig(WBTCConfigBefore, allConfigsAfter);
     }
 
