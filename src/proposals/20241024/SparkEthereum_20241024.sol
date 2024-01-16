e6106ace252515cc5e97233e47dc726282261af4
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-01-16 15:36:29 +0100

    feat: update addresses; update links

--- src/proposals/20241024/SparkEthereum_20241024.sol
+++ src/proposals/20241024/SparkEthereum_20241024.sol
@@ -6,9 +6,11 @@ import { SparkPayloadEthereum, IEngine, Rates, EngineFlags, Address } from '../.
 /**
  * @title  January 24, 2024 Spark Ethereum Proposal - Raise WBTC supply cap, update USDC & USDT oracle to fixed price oracle, update DAI, USDC & USDT IRM to DSR-tracking IRM
  * @author Phoenix Labs
- * @dev This proposal sets WBTC supplyCap, USDC & USDT priceFeed and DAI, USDC & USDT reserveInterestRateStrategy
- * Forum: TBA
- * Vote:  TBA
+ * @dev    This proposal sets WBTC supplyCap, USDC & USDT priceFeed and DAI, USDC & USDT reserveInterestRateStrategy
+ * Forum:        https://forum.makerdao.com/t/jan-10-2024-proposed-changes-to-sparklend-for-upcoming-spell
+ * WBTC Vote:    https://vote.makerdao.com/polling/Qmc3NjZA
+ * Oracles Vote: https://vote.makerdao.com/polling/QmTauEqL
+ * IRM Vote:     https://vote.makerdao.com/polling/QmNrXB9P
  */
 contract SparkEthereum_20241024 is SparkPayloadEthereum {
 
@@ -17,9 +19,9 @@ contract SparkEthereum_20241024 is SparkPayloadEthereum {
     address public constant DAI                = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address public constant USDC               = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address public constant USDT               = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
-    address public constant DAI_IRM            = 0x7d8f2210FAD012E7d260C3ddBeCaCfd48277455F;  // TBA (these are placeholders)
-    address public constant USDC_IRM           = 0xbc8A68B0ab0617D7c90d15bb1601B25d795Dc4c8;  // TBA (these are placeholders)
-    address public constant USDT_IRM           = 0xbc8A68B0ab0617D7c90d15bb1601B25d795Dc4c8;  // TBA (these are placeholders)
+    address public constant DAI_IRM            = 0x7d8f2210FAD012E7d260C3ddBeCaCfd48277455F;  // TBA (this is a placeholder)
+    address public constant USDC_IRM           = 0x0F1a9a787b4103eF5929121CD9399224c6455dD6;
+    address public constant USDT_IRM           = 0x0F1a9a787b4103eF5929121CD9399224c6455dD6;
     address public constant WBTC               = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
     address public constant FIXED_PRICE_ORACLE = 0x42a03F81dd8A1cEcD746dc262e4d1CD9fD39F777;
 
