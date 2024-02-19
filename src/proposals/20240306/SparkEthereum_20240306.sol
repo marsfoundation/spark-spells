a281dacdbfeb352c4463bbf2388db30c02ccfa5b
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-02-19 17:35:33 -0600

    feat: initialize spell files

---
+++ src/proposals/20240306/SparkEthereum_20240306.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.10;
+
+import { IACLManager } from 'aave-v3-core/contracts/interfaces/IACLManager.sol';
+
+/**
+ * @title  March 06, 2024 Spark Ethereum Proposal - Activate Cap Automator
+ * @author Phoenix Labs
+ * @dev    This proposal activates the Cap Automator
+ * Forum:  TODO
+ * Vote:   TODO
+ */
+contract SparkEthereum_20240306 is SparkPayloadEthereum {
+
+    address public constant ACL_MANAGER   = 0xdA135Cd78A086025BcdC87B038a1C462032b510C;
+    address public constant CAP_AUTOMATOR = 0xdA135Cd78A086025BcdC87B038a1C462032b510C; // TODO: Replace with the actual address
+
+    function _postExecute() internal override {
+        IACLManager(ACL_MANAGER).addRiskAdmin(CAP_AUTOMATOR);
+    }
+
+}
