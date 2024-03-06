a4c9a1b23e49782846e29eb75b49e82a815d8eeb
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-03-06 16:58:29 +0100

    [SC-337] March 06 Spell (#34)
    * scaffolding of spell
    
    * finish spell (still missing forum/poll)
    
    * add forum post for spread change
    
    * review fixes
    
    * re-enabled diff and removed now unused dai-specific irm stuff
    
    * feat: add CapAutomator interface
    
    * feat: temporarily add CapAutomator code
    
    * fix: fix minor issues
    
    * feat: initialize spell files
    
    * fix: import SparkPayloadEthereum
    
    * fix: make ad hoc fixes for CapAutomator environment to work
    
    * fix: temp fix
    
    * add poll
    
    * comment change
    
    * add deployed spell
    
    * test: setup tests
    
    * Revert "fix: temp fix"
    
    This reverts commit d95cdab9c5673ba1693274a9b30f1cc1cf0c333a.
    
    * Revert "fix: make ad hoc fixes for CapAutomator environment to work"
    
    This reverts commit 32ea139dd661813aca06e23cc6a4607acd10d43b.
    
    * Revert "fix: import SparkPayloadEthereum"
    
    This reverts commit bc5ed28e1783bf48a1d9fb07b46b115729f6dce7.
    
    * Revert "fix: fix minor issues"
    
    This reverts commit 3d3ebf4efd62c11e19d1c18a7e902cedc71eada9.
    
    * Revert "feat: temporarily add CapAutomator code"
    
    This reverts commit 9e3aa2f59e181c6fc64766e4186b5abe84778008.
    
    * fix: fix the setup
    
    * test: add basic test setup
    
    * refactor: move stale spell into archive
    
    * feat: work on the spell and tests
    
    * test: add extra test
    
    * docs: improve description
    
    * refactor: add minor refactorings
    
    * feat: add interest rate updates
    
    * refactor: remove empty lines
    
    * refactor: add extra lines
    
    * refactor: move old spell into archive
    
    * refactor: add minor refactorings
    
    * refactor: update diff
    
    * refactor: minor improvements
    
    * fix: change uint256 to uint48 where needed
    
    * fix: improve the IRS testing
    
    * refactor: minor refactoring
    
    * change to loop; test each supply and borrow cap with hardcoded values
    
    * add votes to spell comments
    
    * add deployed spell
    
    ---------
    
    Co-authored-by: Sam MacPherson <xylom44@gmail.com>

---
+++ src/proposals/20240306/SparkEthereum_20240306.sol
@@ -0,0 +1,157 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.10;
+
+import { IACLManager } from 'aave-v3-core/contracts/interfaces/IACLManager.sol';
+
+import { ICapAutomator } from '../../interfaces/ICapAutomator.sol';
+
+import { EngineFlags, IEngine,  Rates, SparkPayloadEthereum } from '../../SparkPayloadEthereum.sol';
+
+/**
+ * @title  March 06, 2024 Spark Ethereum Proposal - Activate Cap Automator, update ETH eMode, update interest rates for WETH and rETH, update collateral parameters for multiple markets
+ * @author Phoenix Labs
+ * @dev    This proposal adds the Cap Automator as a risk admin to the Pool and sets supply and borrow cap configs for rETH, sDAI, USDC, USDT, WBTC, WETH and wstETH markets;
+ *         sets ETH eMode to 93% ltv, 95% liqThreshold and 101% liqBonus; updates rETH baseVariableBorrowRate to 0.25% and WETH variableRateSlope1 to 2.8%;
+ *         updates ltv, liqThreshold and liqBonus for rETH, sDAI, WBTC, WETH and wstETH markets.
+ * Forum:  https://forum.makerdao.com/t/feb-22-2024-proposed-changes-to-sparklend-for-upcoming-spell/23739
+ * Votes:  https://vote.makerdao.com/polling/QmUE5xr8#poll-detail
+ *         https://vote.makerdao.com/polling/QmRU6mmi#poll-detail
+ */
+contract SparkEthereum_20240306 is SparkPayloadEthereum {
+
+    address internal constant ACL_MANAGER   = 0xdA135Cd78A086025BcdC87B038a1C462032b510C;
+    address internal constant CAP_AUTOMATOR = 0x2276f52afba7Cf2525fd0a050DF464AC8532d0ef;
+
+    address internal constant RETH   = 0xae78736Cd615f374D3085123A210448E74Fc6393;
+    address internal constant SDAI   = 0x83F20F44975D03b1b09e64809B757c47f942BEeA;
+    address internal constant USDC   = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address internal constant USDT   = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+    address internal constant WBTC   = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
+    address internal constant WETH   = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address internal constant WSTETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
+
+    function _preExecute()
+        internal override
+    {
+        LISTING_ENGINE.POOL_CONFIGURATOR().setEModeCategory(
+            1,
+            92_00,
+            93_00,
+            101_00,
+            address(0),
+            'ETH'
+        );
+    }
+
+    function collateralsUpdates()
+        public pure override returns (IEngine.CollateralUpdate[] memory)
+    {
+        IEngine.CollateralUpdate[] memory collateralUpdates = new IEngine.CollateralUpdate[](5);
+
+        collateralUpdates[0] = IEngine.CollateralUpdate({
+            asset:          RETH,
+            ltv:            79_00,
+            liqThreshold:   80_00,
+            liqBonus:       EngineFlags.KEEP_CURRENT,
+            debtCeiling:    EngineFlags.KEEP_CURRENT,
+            liqProtocolFee: EngineFlags.KEEP_CURRENT,
+            eModeCategory:  EngineFlags.KEEP_CURRENT
+        });
+
+        collateralUpdates[1] = IEngine.CollateralUpdate({
+            asset:          SDAI,
+            ltv:            79_00,
+            liqThreshold:   80_00,
+            liqBonus:       5_00,
+            debtCeiling:    EngineFlags.KEEP_CURRENT,
+            liqProtocolFee: EngineFlags.KEEP_CURRENT,
+            eModeCategory:  EngineFlags.KEEP_CURRENT
+        });
+
+        collateralUpdates[2] = IEngine.CollateralUpdate({
+            asset:          WBTC,
+            ltv:            74_00,
+            liqThreshold:   EngineFlags.KEEP_CURRENT,
+            liqBonus:       EngineFlags.KEEP_CURRENT,
+            debtCeiling:    EngineFlags.KEEP_CURRENT,
+            liqProtocolFee: EngineFlags.KEEP_CURRENT,
+            eModeCategory:  EngineFlags.KEEP_CURRENT
+        });
+
+        collateralUpdates[3] = IEngine.CollateralUpdate({
+            asset:          WETH,
+            ltv:            82_00,
+            liqThreshold:   83_00,
+            liqBonus:       EngineFlags.KEEP_CURRENT,
+            debtCeiling:    EngineFlags.KEEP_CURRENT,
+            liqProtocolFee: EngineFlags.KEEP_CURRENT,
+            eModeCategory:  EngineFlags.KEEP_CURRENT
+        });
+
+        collateralUpdates[4] = IEngine.CollateralUpdate({
+            asset:          WSTETH,
+            ltv:            79_00,
+            liqThreshold:   80_00,
+            liqBonus:       EngineFlags.KEEP_CURRENT,
+            debtCeiling:    EngineFlags.KEEP_CURRENT,
+            liqProtocolFee: EngineFlags.KEEP_CURRENT,
+            eModeCategory:  EngineFlags.KEEP_CURRENT
+        });
+
+        return collateralUpdates;
+    }
+
+    function rateStrategiesUpdates()
+        public view override returns (IEngine.RateStrategyUpdate[] memory)
+    {
+        IEngine.RateStrategyUpdate[] memory ratesUpdate = new IEngine.RateStrategyUpdate[](2);
+        Rates.RateStrategyParams memory rethParams = LISTING_ENGINE
+            .RATE_STRATEGIES_FACTORY()
+            .getStrategyDataOfAsset(RETH);
+
+        rethParams.baseVariableBorrowRate = _bpsToRay(25);
+
+        ratesUpdate[0] = IEngine.RateStrategyUpdate({
+            asset:  RETH,
+            params: rethParams
+        });
+
+        Rates.RateStrategyParams memory wethParams = LISTING_ENGINE
+            .RATE_STRATEGIES_FACTORY()
+            .getStrategyDataOfAsset(WETH);
+
+        wethParams.variableRateSlope1 = _bpsToRay(2_80);
+
+        ratesUpdate[1] = IEngine.RateStrategyUpdate({
+            asset:  WETH,
+            params: wethParams
+        });
+
+        return ratesUpdate;
+    }
+
+    function _postExecute()
+        internal override
+    {
+        IACLManager(ACL_MANAGER).addRiskAdmin(CAP_AUTOMATOR);
+
+        ICapAutomator(CAP_AUTOMATOR).setSupplyCapConfig({asset: RETH, max: 80_000, gap: 10_000, increaseCooldown: 12 hours});
+        ICapAutomator(CAP_AUTOMATOR).setBorrowCapConfig({asset: RETH, max: 2_400,  gap: 100,    increaseCooldown: 12 hours});
+
+        ICapAutomator(CAP_AUTOMATOR).setSupplyCapConfig({asset: SDAI, max: 1_000_000_000, gap: 50_000_000, increaseCooldown: 12 hours});
+
+        ICapAutomator(CAP_AUTOMATOR).setBorrowCapConfig({asset: USDC, max: 57_000_000, gap: 6_000_000, increaseCooldown: 12 hours});
+
+        ICapAutomator(CAP_AUTOMATOR).setBorrowCapConfig({asset: USDT, max: 28_500_000, gap: 3_000_000, increaseCooldown: 12 hours});
+
+        ICapAutomator(CAP_AUTOMATOR).setSupplyCapConfig({asset: WBTC, max: 5_000, gap: 500, increaseCooldown: 12 hours});
+        ICapAutomator(CAP_AUTOMATOR).setBorrowCapConfig({asset: WBTC, max: 2_000, gap: 100, increaseCooldown: 12 hours});
+
+        ICapAutomator(CAP_AUTOMATOR).setSupplyCapConfig({asset: WETH, max: 2_000_000, gap: 150_000, increaseCooldown: 12 hours});
+        ICapAutomator(CAP_AUTOMATOR).setBorrowCapConfig({asset: WETH, max: 1_000_000, gap: 10_000,  increaseCooldown: 12 hours});
+
+        ICapAutomator(CAP_AUTOMATOR).setSupplyCapConfig({asset: WSTETH, max: 1_200_000, gap: 50_000, increaseCooldown: 12 hours});
+        ICapAutomator(CAP_AUTOMATOR).setBorrowCapConfig({asset: WSTETH, max: 3_000,     gap: 100,    increaseCooldown: 12 hours});
+    }
+
+}
---
+++ src/proposals/20240306/SparkEthereum_20240306.t.sol
@@ -0,0 +1,473 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.10;
+
+import { DataTypes }                    from "aave-v3-core/contracts/protocol/libraries/types/DataTypes.sol";
+import { IDefaultInterestRateStrategy } from "aave-v3-core/contracts/interfaces/IDefaultInterestRateStrategy.sol";
+import { IScaledBalanceToken }          from "aave-v3-core/contracts/interfaces/IScaledBalanceToken.sol";
+import { ReserveConfiguration }         from "aave-v3-core/contracts/protocol/libraries/configuration/ReserveConfiguration.sol";
+import { WadRayMath }                   from "aave-v3-core/contracts/protocol/libraries/math/WadRayMath.sol";
+
+import { IERC20 } from '../../interfaces/IERC20.sol';
+
+import '../../SparkTestBase.sol';
+
+interface IOwnable {
+    function owner() external view returns (address);
+}
+
+interface IERC20WithDecimals is IERC20 {
+    function decimals() external view returns (uint256);
+}
+
+contract SparkEthereum_20240306Test is SparkEthereumTestBase {
+
+    using ReserveConfiguration for DataTypes.ReserveConfigurationMap;
+    using WadRayMath for uint256;
+
+    constructor() {
+        id = '20240306';
+    }
+
+    function setUp() public {
+        vm.createSelectFork(getChain('mainnet').rpcUrl, 19313423);  // Feb 26, 2024
+        payload = 0xf3449d6D5827F0F6e0eE4a941f058307056D3736;
+
+        loadPoolContext(poolAddressesProviderRegistry.getAddressesProvidersList()[0]);
+    }
+
+    function testEModeUpdate() public {
+        DataTypes.EModeCategory memory eModeBefore = pool.getEModeCategoryData(1);
+
+        assertEq(eModeBefore.ltv,                  90_00);
+        assertEq(eModeBefore.liquidationThreshold, 93_00);
+        assertEq(eModeBefore.liquidationBonus,     101_00);
+        assertEq(eModeBefore.priceSource,          address(0));
+        assertEq(eModeBefore.label,                'ETH');
+
+        GovHelpers.executePayload(vm, payload, executor);
+
+        DataTypes.EModeCategory memory eModeAfter = pool.getEModeCategoryData(1);
+
+        assertEq(eModeAfter.ltv,                  92_00);
+        assertEq(eModeAfter.liquidationThreshold, eModeBefore.liquidationThreshold);
+        assertEq(eModeAfter.liquidationBonus,     eModeBefore.liquidationBonus);
+        assertEq(eModeAfter.priceSource,          eModeBefore.priceSource);
+        assertEq(eModeAfter.label,                eModeBefore.label);
+    }
+
+    function testMarketUpdates() public {
+        ReserveConfig[] memory allConfigsBefore = createConfigurationSnapshot('', pool);
+
+        /*******************/
+        /*** rETH Before ***/
+        /*******************/
+
+        ReserveConfig memory rethConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'rETH');
+
+        assertEq(rethConfigBefore.ltv,                  68_50);
+        assertEq(rethConfigBefore.liquidationThreshold, 79_50);
+        assertEq(rethConfigBefore.liquidationBonus,     107_00);
+
+        IDefaultInterestRateStrategy rethIRSBefore = IDefaultInterestRateStrategy(
+            rethConfigBefore.interestRateStrategy
+        );
+
+        _validateInterestRateStrategy(
+            address(rethIRSBefore),
+            address(rethIRSBefore),
+            InterestStrategyValues({
+                addressesProvider:             address(poolAddressesProvider),
+                optimalUsageRatio:             rethIRSBefore.OPTIMAL_USAGE_RATIO(),
+                optimalStableToTotalDebtRatio: rethIRSBefore.OPTIMAL_STABLE_TO_TOTAL_DEBT_RATIO(),
+                baseStableBorrowRate:          0.070e27,
+                stableRateSlope1:              rethIRSBefore.getStableRateSlope1(),
+                stableRateSlope2:              rethIRSBefore.getStableRateSlope2(),
+                baseVariableBorrowRate:        0,
+                variableRateSlope1:            0.070e27,
+                variableRateSlope2:            3.000e27
+            })
+        );
+
+        /*******************/
+        /*** sDAI Before ***/
+        /*******************/
+
+        ReserveConfig memory sdaiConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'sDAI');
+
+        assertEq(sdaiConfigBefore.ltv,                  74_00);
+        assertEq(sdaiConfigBefore.liquidationThreshold, 76_00);
+        assertEq(sdaiConfigBefore.liquidationBonus,     104_50);
+
+        /*******************/
+        /*** WBTC Before ***/
+        /*******************/
+
+        ReserveConfig memory wbtcConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'WBTC');
+
+        assertEq(wbtcConfigBefore.ltv,                  70_00);
+        assertEq(wbtcConfigBefore.liquidationThreshold, 75_00);
+        assertEq(wbtcConfigBefore.liquidationBonus,     107_00);
+
+        /*******************/
+        /*** WETH Before ***/
+        /*******************/
+
+        ReserveConfig memory wethConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'WETH');
+
+        assertEq(wethConfigBefore.ltv,                  80_00);
+        assertEq(wethConfigBefore.liquidationThreshold, 82_50);
+        assertEq(wethConfigBefore.liquidationBonus,     105_00);
+
+        IDefaultInterestRateStrategy wethIRSBefore = IDefaultInterestRateStrategy(
+            wethConfigBefore.interestRateStrategy
+        );
+
+        _validateInterestRateStrategy(
+            address(wethIRSBefore),
+            address(wethIRSBefore),
+            InterestStrategyValues({
+                addressesProvider:             address(poolAddressesProvider),
+                optimalUsageRatio:             wethIRSBefore.OPTIMAL_USAGE_RATIO(),
+                optimalStableToTotalDebtRatio: wethIRSBefore.OPTIMAL_STABLE_TO_TOTAL_DEBT_RATIO(),
+                baseStableBorrowRate:          0.032e27,
+                stableRateSlope1:              wethIRSBefore.getStableRateSlope1(),
+                stableRateSlope2:              wethIRSBefore.getStableRateSlope2(),
+                baseVariableBorrowRate:        0,
+                variableRateSlope1:            0.032e27,
+                variableRateSlope2:            1.200e27
+            })
+        );
+
+        /*********************/
+        /*** wstETH Before ***/
+        /*********************/
+
+        ReserveConfig memory wstethConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'wstETH');
+
+        assertEq(wstethConfigBefore.ltv,                  68_50);
+        assertEq(wstethConfigBefore.liquidationThreshold, 79_50);
+        assertEq(wstethConfigBefore.liquidationBonus,     107_00);
+
+        /***********************/
+        /*** Execute Payload ***/
+        /***********************/
+
+        GovHelpers.executePayload(vm, payload, executor);
+
+        ReserveConfig[] memory allConfigsAfter = createConfigurationSnapshot('', pool);
+
+        /*******************/
+        /*** rETH After ****/
+        /*******************/
+
+        ReserveConfig memory rethConfigAfter = rethConfigBefore;
+
+        rethConfigAfter.interestRateStrategy = _findReserveConfigBySymbol(allConfigsAfter, 'rETH').interestRateStrategy;
+
+        rethConfigAfter.ltv                  = 79_00;
+        rethConfigAfter.liquidationThreshold = 80_00;
+
+        _validateReserveConfig(rethConfigAfter, allConfigsAfter);
+
+        _validateInterestRateStrategy(
+            rethConfigAfter.interestRateStrategy,
+            rethConfigAfter.interestRateStrategy,
+            InterestStrategyValues({
+                addressesProvider:             address(poolAddressesProvider),
+                optimalUsageRatio:             rethIRSBefore.OPTIMAL_USAGE_RATIO(),
+                optimalStableToTotalDebtRatio: rethIRSBefore.OPTIMAL_STABLE_TO_TOTAL_DEBT_RATIO(),
+                baseStableBorrowRate:          rethIRSBefore.getBaseStableBorrowRate(),
+                stableRateSlope1:              rethIRSBefore.getStableRateSlope1(),
+                stableRateSlope2:              rethIRSBefore.getStableRateSlope2(),
+                baseVariableBorrowRate:        0.0025e27,
+                variableRateSlope1:            rethIRSBefore.getVariableRateSlope1(),
+                variableRateSlope2:            rethIRSBefore.getVariableRateSlope2()
+            })
+        );
+
+        /*******************/
+        /*** sDAI After ****/
+        /*******************/
+
+        ReserveConfig memory sdaiConfigAfter = sdaiConfigBefore;
+
+        sdaiConfigAfter.ltv                  = 79_00;
+        sdaiConfigAfter.liquidationThreshold = 80_00;
+        sdaiConfigAfter.liquidationBonus     = 105_00;
+
+        _validateReserveConfig(sdaiConfigAfter, allConfigsAfter);
+
+        /*******************/
+        /*** WBTC After ****/
+        /*******************/
+
+        ReserveConfig memory wbtcConfigAfter = wbtcConfigBefore;
+
+        wbtcConfigAfter.ltv = 74_00;
+
+        _validateReserveConfig(wbtcConfigAfter, allConfigsAfter);
+
+        /*******************/
+        /*** WETH After ****/
+        /*******************/
+
+        ReserveConfig memory wethConfigAfter = wethConfigBefore;
+
+        wethConfigAfter.interestRateStrategy = _findReserveConfigBySymbol(allConfigsAfter, 'WETH').interestRateStrategy;
+
+        wethConfigAfter.ltv                  = 82_00;
+        wethConfigAfter.liquidationThreshold = 83_00;
+
+        _validateReserveConfig(wethConfigAfter, allConfigsAfter);
+
+        _validateInterestRateStrategy(
+            wethConfigAfter.interestRateStrategy,
+            wethConfigAfter.interestRateStrategy,
+            InterestStrategyValues({
+                addressesProvider:             address(poolAddressesProvider),
+                optimalUsageRatio:             wethIRSBefore.OPTIMAL_USAGE_RATIO(),
+                optimalStableToTotalDebtRatio: wethIRSBefore.OPTIMAL_STABLE_TO_TOTAL_DEBT_RATIO(),
+                baseStableBorrowRate:          0.028e27,
+                stableRateSlope1:              wethIRSBefore.getStableRateSlope1(),
+                stableRateSlope2:              wethIRSBefore.getStableRateSlope2(),
+                baseVariableBorrowRate:        wethIRSBefore.getBaseVariableBorrowRate(),
+                variableRateSlope1:            0.028e27,
+                variableRateSlope2:            wethIRSBefore.getVariableRateSlope2()
+            })
+        );
+
+        /*********************/
+        /*** wstETH After ****/
+        /*********************/
+
+        ReserveConfig memory wstethConfigAfter = wstethConfigBefore;
+
+        wstethConfigAfter.ltv                  = 79_00;
+        wstethConfigAfter.liquidationThreshold = 80_00;
+
+        _validateReserveConfig(wstethConfigAfter, allConfigsAfter);
+    }
+
+    function testCapAutomatorDeploy() public {
+        assertEq(address(capAutomator), 0x2276f52afba7Cf2525fd0a050DF464AC8532d0ef);
+
+        assertEq(address(capAutomator.poolConfigurator()), address(poolConfigurator));
+        assertEq(address(capAutomator.pool()),             address(pool));
+
+        assertEq(IOwnable(address(capAutomator)).owner(), executor);
+    }
+
+    function testCapAutomatorConfiguration() public {
+        assertEq(aclManager.isRiskAdmin(address(capAutomator)), false);
+
+        _assertSupplyCapConfigNotSet(RETH);
+        _assertBorrowCapConfigNotSet(RETH);
+
+        _assertSupplyCapConfigNotSet(SDAI);
+        _assertBorrowCapConfigNotSet(SDAI);
+
+        _assertSupplyCapConfigNotSet(USDC);
+        _assertBorrowCapConfigNotSet(USDC);
+
+        _assertSupplyCapConfigNotSet(USDT);
+        _assertBorrowCapConfigNotSet(USDT);
+
+        _assertSupplyCapConfigNotSet(WBTC);
+        _assertBorrowCapConfigNotSet(WBTC);
+
+        _assertSupplyCapConfigNotSet(WETH);
+        _assertBorrowCapConfigNotSet(WETH);
+
+        _assertSupplyCapConfigNotSet(WSTETH);
+        _assertBorrowCapConfigNotSet(WSTETH);
+
+        GovHelpers.executePayload(vm, payload, executor);
+
+        assertEq(aclManager.isRiskAdmin(address(capAutomator)), true);
+
+        /************/
+        /*** rETH ***/
+        /************/
+
+        _assertSupplyCapConfig({
+            asset:            RETH,
+            max:              80_000,
+            gap:              10_000,
+            increaseCooldown: 12 hours
+        });
+
+        _assertBorrowCapConfig({
+            asset:            RETH,
+            max:              2_400,
+            gap:              100,
+            increaseCooldown: 12 hours
+        });
+
+        /************/
+        /*** sDAI ***/
+        /************/
+
+        _assertSupplyCapConfig({
+            asset:            SDAI,
+            max:              1_000_000_000,
+            gap:              50_000_000,
+            increaseCooldown: 12 hours
+        });
+
+        _assertBorrowCapConfigNotSet(SDAI);
+
+        /************/
+        /*** USDC ***/
+        /************/
+
+        _assertSupplyCapConfigNotSet(USDC);
+
+        _assertBorrowCapConfig({
+            asset:            USDC,
+            max:              57_000_000,
+            gap:              6_000_000,
+            increaseCooldown: 12 hours
+        });
+
+        /************/
+        /*** USDT ***/
+        /************/
+
+        _assertSupplyCapConfigNotSet(USDT);
+
+        _assertBorrowCapConfig({
+            asset:            USDT,
+            max:              28_500_000,
+            gap:              3_000_000,
+            increaseCooldown: 12 hours
+        });
+
+        /************/
+        /*** WBTC ***/
+        /************/
+
+        _assertSupplyCapConfig({
+            asset:            WBTC,
+            max:              5_000,
+            gap:              500,
+            increaseCooldown: 12 hours
+        });
+
+        _assertBorrowCapConfig({
+            asset:            WBTC,
+            max:              2_000,
+            gap:              100,
+            increaseCooldown: 12 hours
+        });
+
+        /************/
+        /*** WETH ***/
+        /************/
+
+        _assertSupplyCapConfig({
+            asset:            WETH,
+            max:              2_000_000,
+            gap:              150_000,
+            increaseCooldown: 12 hours
+        });
+
+        _assertBorrowCapConfig({
+            asset:            WETH,
+            max:              1_000_000,
+            gap:              10_000,
+            increaseCooldown: 12 hours
+        });
+
+        /**************/
+        /*** wstETH ***/
+        /**************/
+
+        _assertSupplyCapConfig({
+            asset:            WSTETH,
+            max:              1_200_000,
+            gap:              50_000,
+            increaseCooldown: 12 hours
+        });
+
+        _assertBorrowCapConfig({
+            asset:            WSTETH,
+            max:              3_000,
+            gap:              100,
+            increaseCooldown: 12 hours
+        });
+    }
+
+    function testCapAutomatorCapUpdates() public {
+        GovHelpers.executePayload(vm, payload, executor);
+
+        address[] memory reserves = pool.getReservesList();
+        for (uint256 i = 0; i < reserves.length; i++) {
+            _assertAutomatedCapsUpdate(reserves[i]);
+        }
+
+        assertEq(pool.getReserveData(RETH).configuration.getSupplyCap(),   54_089);
+        assertEq(pool.getReserveData(RETH).configuration.getBorrowCap(),   112);
+        assertEq(pool.getReserveData(SDAI).configuration.getSupplyCap(),   133_736_696);
+        assertEq(pool.getReserveData(SDAI).configuration.getBorrowCap(),   0);
+        assertEq(pool.getReserveData(USDC).configuration.getSupplyCap(),   60_000_000);
+        assertEq(pool.getReserveData(USDC).configuration.getBorrowCap(),   6_705_942);
+        assertEq(pool.getReserveData(USDT).configuration.getSupplyCap(),   30_000_000);
+        assertEq(pool.getReserveData(USDT).configuration.getBorrowCap(),   3_108_977);
+        assertEq(pool.getReserveData(WBTC).configuration.getSupplyCap(),   5_000);
+        assertEq(pool.getReserveData(WBTC).configuration.getBorrowCap(),   181);
+        assertEq(pool.getReserveData(WETH).configuration.getSupplyCap(),   389_811);
+        assertEq(pool.getReserveData(WETH).configuration.getBorrowCap(),   144_646);
+        assertEq(pool.getReserveData(WSTETH).configuration.getSupplyCap(), 742_823);
+        assertEq(pool.getReserveData(WSTETH).configuration.getBorrowCap(), 214);
+        assertEq(pool.getReserveData(DAI).configuration.getSupplyCap(),    0);
+        assertEq(pool.getReserveData(DAI).configuration.getBorrowCap(),    0);
+    }
+
+    function _assertAutomatedCapsUpdate(address asset) internal {
+        DataTypes.ReserveData memory reserveDataBefore = pool.getReserveData(asset);
+
+        uint256 supplyCapBefore = reserveDataBefore.configuration.getSupplyCap();
+        uint256 borrowCapBefore = reserveDataBefore.configuration.getBorrowCap();
+
+        capAutomator.exec(asset);
+
+        DataTypes.ReserveData memory reserveDataAfter = pool.getReserveData(asset);
+
+        uint256 supplyCapAfter = reserveDataAfter.configuration.getSupplyCap();
+        uint256 borrowCapAfter = reserveDataAfter.configuration.getBorrowCap();
+
+        uint48 max;
+        uint48 gap;
+
+        (max, gap,,,) = capAutomator.supplyCapConfigs(asset);
+
+        if (max > 0) {
+            uint256 currentSupply = (IScaledBalanceToken(reserveDataAfter.aTokenAddress).scaledTotalSupply() + uint256(reserveDataAfter.accruedToTreasury))
+                .rayMul(reserveDataAfter.liquidityIndex)
+                / 10 ** IERC20WithDecimals(reserveDataAfter.aTokenAddress).decimals();
+
+            uint256 expectedSupplyCap = uint256(max) < currentSupply + uint256(gap)
+                ? uint256(max)
+                : currentSupply + uint256(gap);
+
+            assertEq(supplyCapAfter, expectedSupplyCap);
+        } else {
+            assertEq(supplyCapAfter, supplyCapBefore);
+        }
+
+        (max, gap,,,) = capAutomator.borrowCapConfigs(asset);
+
+        if (max > 0) {
+            uint256 currentBorrows = IERC20(reserveDataAfter.variableDebtTokenAddress).totalSupply() / 10 ** IERC20WithDecimals(reserveDataAfter.variableDebtTokenAddress).decimals();
+
+            uint256 expectedBorrowCap = uint256(max) < currentBorrows + uint256(gap)
+                ? uint256(max)
+                : currentBorrows + uint256(gap);
+
+            assertEq(borrowCapAfter, expectedBorrowCap);
+        } else {
+            assertEq(borrowCapAfter, borrowCapBefore);
+        }
+    }
+
+}
