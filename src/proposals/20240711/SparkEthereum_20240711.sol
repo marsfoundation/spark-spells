ebd6e2036874339e4c99dfd54a357a181df5dab5
shaito <shaito@defi.sucks>
2024-07-04 16:29:22 -0300

    feat: update dai borrow rate

--- src/proposals/20240711/SparkEthereum_20240711.sol
+++ src/proposals/20240711/SparkEthereum_20240711.sol
@@ -13,6 +13,13 @@ import { SparkPayloadEthereum, Ethereum, IEngine, EngineFlags } from 'src/SparkP
  */
 contract SparkEthereum_20240711 is SparkPayloadEthereum {
 
+    // Per-second APY for DSR comes from: https://github.com/makerdao/spells-mainnet/blob/master/src/test/rates.sol
+    // Formula for 7% target DSR APY (0.067658648546393647164576000)
+    // bc -l <<< 'scale=27; (1.000000002145441671308778766 - 1) * 60 * 60 * 24 * 365'
+    // Formula for 8% target APY (0.009302392683643256181504000 spread at current DSR):
+    // bc -l <<< 'scale=27; (e( l(1.08)/(60 * 60 * 24 * 365) ) - 1) * 60 * 60 * 24 * 365 - 0.067658648546393647164576000'
+    address internal constant DAI_IRM = 0x92af90912FD747aE836e0E9d5462A210EfE6A881;
+
     function collateralsUpdates()
         public pure override returns (IEngine.CollateralUpdate[] memory)
     {
@@ -35,8 +42,13 @@ contract SparkEthereum_20240711 is SparkPayloadEthereum {
     function _postExecute()
         internal override
     {   
+        // Decrease DAI rates by 1 percentage point, from 8% to 7%
+        LISTING_ENGINE.POOL_CONFIGURATOR().setReserveInterestRateStrategyAddress(
+            Ethereum.DAI,
+            DAI_IRM
+        );
+        
         // Increase max supply cap to 200,000 weETH (Increase for 150,000 weETH)
         ICapAutomator(Ethereum.CAP_AUTOMATOR).setSupplyCapConfig({asset: Ethereum.WEETH, max: 200_000, gap: 5_000, increaseCooldown: 12 hours});
     }
-
 }
--- src/proposals/20240711/SparkEthereum_20240711.t.sol
+++ src/proposals/20240711/SparkEthereum_20240711.t.sol
@@ -3,17 +3,44 @@ pragma solidity ^0.8.10;
 
 import 'src/SparkTestBase.sol';
 
+interface IPotRateSource {
+    function pot() external view returns (address);
+}
+
+interface IIRM {
+    function RATE_SOURCE() external view returns (IPotRateSource);
+}
+
+interface IRateSource {
+    function getAPR() external view returns (int256);
+}
+
+interface PotLike {
+    function drip() external;
+    function file(bytes32 what, uint256 data) external;
+}
+
 contract SparkEthereum_20240711Test is SparkEthereumTestBase {
+
+    address public constant OLD_DAI_INTEREST_RATE_STRATEGY = 0x5ae77aE8ec1B0F9a741C80A4Cdb876e6b5B619b9;
+    address public constant NEW_DAI_INTEREST_RATE_STRATEGY = 0x92af90912FD747aE836e0E9d5462A210EfE6A881;
+
+    int256 public constant DAI_IRM_SPREAD = 0.009302392683643256181504000e27;
 
     constructor() {
         id = '20240711';
     }
 
     function setUp() public {
-        vm.createSelectFork(getChain('mainnet').rpcUrl, 20185330);
+        vm.createSelectFork(getChain('mainnet').rpcUrl, 20235177);
         payload = deployPayload();
 
         loadPoolContext(poolAddressesProviderRegistry.getAddressesProvidersList()[0]);
+
+        vm.startPrank(Ethereum.PAUSE_PROXY);
+        PotLike(Ethereum.POT).drip();
+        PotLike(Ethereum.POT).file('dsr', 1000000002145441671308778766);
+        vm.stopPrank();
     }
 
     function testCapIncrease() public {
@@ -56,5 +83,49 @@ contract SparkEthereum_20240711Test is SparkEthereumTestBase {
         // The rest of the configuration should remain the same
         weethConfigBefore.debtCeiling = weethConfigAfter.debtCeiling;
         _validateReserveConfig(weethConfigBefore, allConfigsAfter);
+    }
+
+    function testDaiInterestRateUpdate() public {
+        ReserveConfig[] memory allConfigsBefore = createConfigurationSnapshot('', pool);
+
+        ReserveConfig memory daiConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'DAI');
+
+        assertEq(daiConfigBefore.interestRateStrategy, OLD_DAI_INTEREST_RATE_STRATEGY);
+
+        executePayload(payload);
+
+        ReserveConfig[] memory allConfigsAfter = createConfigurationSnapshot('', pool);
+
+        ReserveConfig memory daiConfigAfter = _findReserveConfigBySymbol(allConfigsAfter, 'DAI');
+
+        address rateSource = address(IIRM(daiConfigAfter.interestRateStrategy).RATE_SOURCE());
+        address pot = IPotRateSource(rateSource).pot();
+        assertEq(pot, IIRM(OLD_DAI_INTEREST_RATE_STRATEGY).RATE_SOURCE().pot());  // Same pot as before
+
+        int256 potDsrApr = IRateSource(rateSource).getAPR();
+
+        // Approx 7% APY
+        assertEq(_getAPY(uint256(potDsrApr)), 0.069999999999999999987440814e27);
+
+        uint256 expectedDaiBaseVariableBorrowRate = uint256(potDsrApr + DAI_IRM_SPREAD);
+
+        // Approx 8% APY
+        assertEq(_getAPY(expectedDaiBaseVariableBorrowRate), 0.079999999999999999951590734e27);
+
+        _validateInterestRateStrategy(
+            daiConfigAfter.interestRateStrategy,
+            NEW_DAI_INTEREST_RATE_STRATEGY,
+            InterestStrategyValues({
+                addressesProvider:             address(poolAddressesProvider),
+                optimalUsageRatio:             1e27,
+                optimalStableToTotalDebtRatio: 0,
+                baseStableBorrowRate:          0,
+                stableRateSlope1:              0,
+                stableRateSlope2:              0,
+                baseVariableBorrowRate:        expectedDaiBaseVariableBorrowRate,
+                variableRateSlope1:            0,
+                variableRateSlope2:            0
+            })
+        );
     }
 }
