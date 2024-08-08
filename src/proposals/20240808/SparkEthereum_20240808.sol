145df1e71fca6b7c6fb0474c713d4e5362ba9e6a
Sam MacPherson <xylom44@gmail.com>
2024-08-08 09:20:04 -0400

    add rate reduction for DAI

--- src/proposals/20240808/SparkEthereum_20240808.sol
+++ src/proposals/20240808/SparkEthereum_20240808.sol
@@ -12,11 +12,21 @@ import { Ethereum, SparkPayloadEthereum } from 'src/SparkPayloadEthereum.sol';
  */
 contract SparkEthereum_20240808 is SparkPayloadEthereum {
 
+    // Per-second APY for DSR comes from: https://github.com/makerdao/spells-mainnet/blob/master/src/test/rates.sol
+    // Formula for 6% target DSR APY (0.058268908177807359323232000)
+    // bc -l <<< 'scale=27; (1.000000001847694957439350562 - 1) * 60 * 60 * 24 * 365'
+    // Formula for 7% target APY (0.009389740368586287841344000 spread at current DSR):
+    // bc -l <<< 'scale=27; (e( l(1.07)/(60 * 60 * 24 * 365) ) - 1) * 60 * 60 * 24 * 365 - 0.058268908177807359323232000'
+    address internal constant DAI_IRM  = 0xC527A1B514796A6519f236dd906E73cab5aA2E71;
     address internal constant WETH_IRM = 0x6fd32465a23aa0DBaE0D813B7157D8CB2b08Dae4;
 
     function _postExecute()
         internal override
     {
+        LISTING_ENGINE.POOL_CONFIGURATOR().setReserveInterestRateStrategyAddress(
+            Ethereum.DAI,
+            DAI_IRM
+        );
         LISTING_ENGINE.POOL_CONFIGURATOR().setReserveInterestRateStrategyAddress(
             Ethereum.WETH,
             WETH_IRM
--- src/proposals/20240808/SparkEthereum_20240808.t.sol
+++ src/proposals/20240808/SparkEthereum_20240808.t.sol
@@ -3,6 +3,19 @@ pragma solidity ^0.8.10;
 
 import 'src/SparkTestBase.sol';
 
+interface IPotRateSource {
+    function pot() external view returns (address);
+}
+
+interface IIRM {
+    function RATE_SOURCE() external view returns (IPotRateSource);
+}
+
+interface PotLike {
+    function drip() external;
+    function file(bytes32 what, uint256 data) external;
+}
+
 interface IRateSource {
     function getAPR() external view returns (uint256);
     function decimals() external view returns (uint8);
@@ -25,24 +38,34 @@ contract SparkEthereum_20240808Test is SparkEthereumTestBase {
     address public constant LST_SOURCE              = 0x08669C836F41AEaD03e3EF81a59f3b8e72EC417A;
     address public constant CAPPED_FALLBACK_WRAPPER = 0xaBc99f366D2bE1f4e5b8DFC0F561a751dd836246;
 
+    address public constant OLD_DAI_INTEREST_RATE_STRATEGY = 0x92af90912FD747aE836e0E9d5462A210EfE6A881;
+    address public constant NEW_DAI_INTEREST_RATE_STRATEGY = 0xC527A1B514796A6519f236dd906E73cab5aA2E71;
+
     address public constant OLD_WETH_INTEREST_RATE_STRATEGY = 0xE27c3f9d35e00ae48144b35DD157F72AaF36c77e;
     address public constant NEW_WETH_INTEREST_RATE_STRATEGY = 0x6fd32465a23aa0DBaE0D813B7157D8CB2b08Dae4;
 
     // Not a special number, the APR just happens to be 3.0%
-    uint256 public constant LST_ORACLE_YIELD = 0.030118698349614199e18;
+    uint256 public constant LST_ORACLE_YIELD = 0.030465303564745507e18;
+
+    uint256 public constant DAI_IRM_SPREAD = 0.009389740368586287841344000e27;
 
     constructor() {
         id = '20240808';
     }
 
     function setUp() public {
-        vm.createSelectFork(getChain('mainnet').rpcUrl, 20435132);  // Aug 1, 2024
-        payload = 0xAFDf518d97DEA3420f007Deea2F9fBa0a28B3227;
+        vm.createSelectFork(getChain('mainnet').rpcUrl, 20484105);  // Aug 8, 2024
+        payload = deployPayload();
 
         loadPoolContext(poolAddressesProviderRegistry.getAddressesProvidersList()[0]);
+
+        vm.startPrank(Ethereum.PAUSE_PROXY);
+        PotLike(Ethereum.POT).drip();
+        PotLike(Ethereum.POT).file('dsr', 1000000001847694957439350562);
+        vm.stopPrank();
     }
 
-    function testOracleDeployment() public {
+    function testLSTOracleDeployment() public {
         vm.prank(address(0));  // Whitelist allows address(0)
         assertEq(IRateSource(LST_SOURCE).getAPR(),   LST_ORACLE_YIELD);
         assertEq(IRateSource(LST_SOURCE).decimals(), 18);
@@ -58,7 +81,51 @@ contract SparkEthereum_20240808Test is SparkEthereumTestBase {
         assertEq(IKinkedIRM(NEW_WETH_INTEREST_RATE_STRATEGY).getVariableRateSlope1Spread(), 0);
     }
 
-    function testSpellSpecifics() public {
+    function testDaiInterestRateUpdate() public {
+        ReserveConfig[] memory allConfigsBefore = createConfigurationSnapshot('', pool);
+
+        ReserveConfig memory daiConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'DAI');
+
+        assertEq(daiConfigBefore.interestRateStrategy, OLD_DAI_INTEREST_RATE_STRATEGY);
+
+        executePayload(payload);
+
+        ReserveConfig[] memory allConfigsAfter = createConfigurationSnapshot('', pool);
+
+        ReserveConfig memory daiConfigAfter = _findReserveConfigBySymbol(allConfigsAfter, 'DAI');
+
+        address rateSource = address(IIRM(daiConfigAfter.interestRateStrategy).RATE_SOURCE());
+        address pot = IPotRateSource(rateSource).pot();
+        assertEq(pot, IIRM(OLD_DAI_INTEREST_RATE_STRATEGY).RATE_SOURCE().pot());  // Same pot as before
+
+        uint256 potDsrApr = IRateSource(rateSource).getAPR();
+
+        // Approx 6% APY
+        assertEq(_getAPY(potDsrApr), 0.059999999999999999957390146e27);
+
+        uint256 expectedDaiBaseVariableBorrowRate = potDsrApr + DAI_IRM_SPREAD;
+
+        // Approx 7% APY
+        assertEq(_getAPY(expectedDaiBaseVariableBorrowRate), 0.069999999999999999987440814e27);
+
+        _validateInterestRateStrategy(
+            daiConfigAfter.interestRateStrategy,
+            NEW_DAI_INTEREST_RATE_STRATEGY,
+            InterestStrategyValues({
+                addressesProvider:             address(poolAddressesProvider),
+                optimalUsageRatio:             1e27,
+                optimalStableToTotalDebtRatio: 0,
+                baseStableBorrowRate:          0,
+                stableRateSlope1:              0,
+                stableRateSlope2:              0,
+                baseVariableBorrowRate:        expectedDaiBaseVariableBorrowRate,
+                variableRateSlope1:            0,
+                variableRateSlope2:            0
+            })
+        );
+    }
+
+    function testWethInterestRateUpdate() public {
         ReserveConfig[] memory allConfigsBefore = createConfigurationSnapshot('', pool);
 
         ReserveConfig memory wethConfigBefore = _findReserveConfigBySymbol(allConfigsBefore, 'WETH');
@@ -72,7 +139,7 @@ contract SparkEthereum_20240808Test is SparkEthereumTestBase {
         ReserveConfig memory wethConfigAfter = _findReserveConfigBySymbol(allConfigsAfter, 'WETH');
 
         uint256 expectedSlope1 = LST_ORACLE_YIELD * 10 ** 9;
-        assertEq(expectedSlope1, 0.030118698349614199e27);
+        assertEq(expectedSlope1, 0.030465303564745507e27);
 
         _validateInterestRateStrategy(
             wethConfigAfter.interestRateStrategy,
