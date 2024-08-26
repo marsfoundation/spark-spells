6d2c84b8787eaf7b8e5b9fcb6d78d50643aab163
shaito <shaito@defi.sucks>
2024-08-26 15:19:34 -0300

    better assignment spacing

--- src/proposals/20240905/SparkEthereum_20240905.sol
+++ src/proposals/20240905/SparkEthereum_20240905.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.10;
 
-import { Ethereum, SparkPayloadEthereum, IEngine, EngineFlags } from 'src/SparkPayloadEthereum.sol';
+import { Ethereum, SparkPayloadEthereum, IEngine } from 'src/SparkPayloadEthereum.sol';
 import { IKillSwitchOracle } from 'lib/sparklend-kill-switch/src/interfaces/IKillSwitchOracle.sol';
-import {IV3RateStrategyFactory as Rates} from '../../interfaces/IV3RateStrategyFactory.sol';
 import { IAaveOracle } from "aave-v3-core/contracts/interfaces/IAaveOracle.sol";
 
 /**
@@ -12,16 +11,17 @@ import { IAaveOracle } from "aave-v3-core/contracts/interfaces/IAaveOracle.sol";
  *         Remove WBTC/BTC from kill switch
  * @author Wonderland
  * Forum:  https://forum.makerdao.com/t/aug-23-2024-proposal-changes-to-spark-for-upcoming-spell/24940
- * Vote:  TODO
+ * Vote:   https://vote.makerdao.com/polling/QmW55juU
+ *         https://vote.makerdao.com/polling/QmQa73Cc
  */
 contract SparkEthereum_20240905 is SparkPayloadEthereum {
 
-    address internal constant WETH_ORACLE = 0xf07ca0e66A798547E4CB3899EC592e1E99Ef6Cb3;
-    address internal constant WSTETH_ORACLE = 0xf77e132799DBB0d83A4fB7df10DA04849340311A;
-    address internal constant RETH_ORACLE = 0x11af58f13419fD3ce4d3A90372200c80Bc62f140;
-    address internal constant WEETH_ORACLE = 0x28897036f8459bFBa886083dD6b4Ce4d2f14a57F;
+    address internal constant WETH_ORACLE     =    0xf07ca0e66A798547E4CB3899EC592e1E99Ef6Cb3;
+    address internal constant WSTETH_ORACLE   =    0xf77e132799DBB0d83A4fB7df10DA04849340311A;
+    address internal constant RETH_ORACLE     =    0x11af58f13419fD3ce4d3A90372200c80Bc62f140;
+    address internal constant WEETH_ORACLE    =    0x28897036f8459bFBa886083dD6b4Ce4d2f14a57F;
 
-    address internal constant WBTC_BTC_ORACLE  = 0xfdFD9C85aD200c506Cf9e21F1FD8dd01932FBB23;
+    address internal constant WBTC_BTC_ORACLE =    0xfdFD9C85aD200c506Cf9e21F1FD8dd01932FBB23;
     
     function priceFeedsUpdates() public pure override returns (IEngine.PriceFeedUpdate[] memory) {
         IEngine.PriceFeedUpdate[] memory updates = new IEngine.PriceFeedUpdate[](3);
@@ -49,11 +49,11 @@ contract SparkEthereum_20240905 is SparkPayloadEthereum {
         IKillSwitchOracle(Ethereum.KILL_SWITCH_ORACLE).disableOracle(WBTC_BTC_ORACLE);
 
         // Change the WETH oracle to the aggor one
-        address[] memory assets = new address[](1);
-        address[] memory sources = new address[](1);
+        address[] memory assets     =   new address[](1);
+        address[] memory sources    =   new address[](1);
 
-        assets[0] = Ethereum.WETH;
-        sources[0] = WETH_ORACLE;
+        assets[0]   =   Ethereum.WETH;
+        sources[0]  =   WETH_ORACLE;
 
         IAaveOracle(Ethereum.AAVE_ORACLE).setAssetSources(assets, sources);
     }
--- src/proposals/20240905/SparkEthereum_20240905.t.sol
+++ src/proposals/20240905/SparkEthereum_20240905.t.sol
@@ -11,17 +11,17 @@ interface IOracle {
 
 contract SparkEthereum_20240905Test is SparkEthereumTestBase {
 
-    address internal constant WETH_ORACLE_OLD =   0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
-    address internal constant WSTETH_ORACLE_OLD = 0x8B6851156023f4f5A66F68BEA80851c3D905Ac93;
-    address internal constant RETH_ORACLE_OLD =   0x05225Cd708bCa9253789C1374e4337a019e99D56;
-    address internal constant WEETH_ORACLE_OLD =  0x1A6BDB22b9d7a454D20EAf12DB55D6B5F058183D;
+    address internal constant WETH_ORACLE_OLD   =   0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
+    address internal constant WSTETH_ORACLE_OLD =   0x8B6851156023f4f5A66F68BEA80851c3D905Ac93;
+    address internal constant RETH_ORACLE_OLD   =   0x05225Cd708bCa9253789C1374e4337a019e99D56;
+    address internal constant WEETH_ORACLE_OLD  =   0x1A6BDB22b9d7a454D20EAf12DB55D6B5F058183D;
 
-    address internal constant WETH_ORACLE = 0xf07ca0e66A798547E4CB3899EC592e1E99Ef6Cb3;
-    address internal constant WSTETH_ORACLE = 0xf77e132799DBB0d83A4fB7df10DA04849340311A;
-    address internal constant RETH_ORACLE = 0x11af58f13419fD3ce4d3A90372200c80Bc62f140;
-    address internal constant WEETH_ORACLE = 0x28897036f8459bFBa886083dD6b4Ce4d2f14a57F;
+    address internal constant WETH_ORACLE       =   0xf07ca0e66A798547E4CB3899EC592e1E99Ef6Cb3;
+    address internal constant WSTETH_ORACLE     =   0xf77e132799DBB0d83A4fB7df10DA04849340311A;
+    address internal constant RETH_ORACLE       =   0x11af58f13419fD3ce4d3A90372200c80Bc62f140;
+    address internal constant WEETH_ORACLE      =   0x28897036f8459bFBa886083dD6b4Ce4d2f14a57F;
 
-    address internal constant WBTC_BTC_ORACLE  = 0xfdFD9C85aD200c506Cf9e21F1FD8dd01932FBB23;
+    address internal constant WBTC_BTC_ORACLE   =   0xfdFD9C85aD200c506Cf9e21F1FD8dd01932FBB23;
 
     constructor() {
         id = '20240905';
