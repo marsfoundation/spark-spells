94157d35920776f00ee3b22aeaa7aa26041d12cd
Sam MacPherson <xylom44@gmail.com>
2024-10-23 09:09:55 -0400

    init new spell

---
+++ src/proposals/20241107/SparkEthereum_20241107.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.10;
+
+import { Ethereum, SparkPayloadEthereum } from 'src/SparkPayloadEthereum.sol';
+
+/**
+ * @title  Nov 7, 2024 Spark Ethereum Proposal
+ * @notice Activate Spark Liquidity Layer
+ * @author Phoenix Labs
+ * Forum:  TODO
+ * Vote:   TODO
+ */
+contract SparkEthereum_20241107 is SparkPayloadEthereum {
+
+    function _postExecute() internal override {
+        
+    }
+}
