218bafdf9c85bb5bbf1cbf8a0c9a2ff0eee1d5b8
Sam MacPherson <xylom44@gmail.com>
2024-10-28 13:07:41 -0400

    add other spell items

--- src/proposals/20241107/SparkEthereum_20241107.sol
+++ src/proposals/20241107/SparkEthereum_20241107.sol
@@ -4,7 +4,11 @@ pragma solidity ^0.8.10;
 import { IERC20 }   from 'forge-std/interfaces/IERC20.sol';
 import { IERC4626 } from 'forge-std/interfaces/IERC4626.sol';
 
-import { Ethereum, SparkPayloadEthereum } from 'src/SparkPayloadEthereum.sol';
+import { Ethereum, SparkPayloadEthereum, IEngine, EngineFlags } from 'src/SparkPayloadEthereum.sol';
+
+import { ICapAutomator } from "lib/sparklend-cap-automator/src/interfaces/ICapAutomator.sol";
+
+import { IMetaMorpho, MarketParams } from 'lib/metamorpho/src/interfaces/IMetaMorpho.sol';
 
 import { Base } from 'spark-address-registry/Base.sol';
 
@@ -41,16 +45,62 @@ interface ITokenBridge {
  */
 contract SparkEthereum_20241107 is SparkPayloadEthereum {
 
-    address constant FREEZER = 0x298b375f24CeDb45e936D7e21d6Eb05e344adFb5;  // Gov. facilitator multisig
-    address constant RELAYER = 0x8a25A24EDE9482C4Fc0738F99611BE58F1c839AB;
+    address internal constant PT_26DEC2024_PRICE_FEED = 0x81E5E28F33D314e9211885d6f0F4080E755e4595;
+    address internal constant PT_SUSDE_26DEC2024      = 0xEe9085fC268F6727d5D4293dBABccF901ffDCC29;
 
-    uint256 constant USDS_MINT_AMOUNT     = 9_000_000e18;
-    uint256 constant SUSDS_DEPOSIT_AMOUNT = 8_000_000e18;
-    uint256 constant USDS_BRIDGE_AMOUNT   = 1_000_000e18;
+    address internal constant WETH_IRM = address(0);  // TODO deploy
 
-    address constant BASE_PAYLOAD = address(0);
+    address internal constant FREEZER = 0x298b375f24CeDb45e936D7e21d6Eb05e344adFb5;  // Gov. facilitator multisig
+    address internal constant RELAYER = 0x8a25A24EDE9482C4Fc0738F99611BE58F1c839AB;
+
+    uint256 internal constant USDS_MINT_AMOUNT     = 9_000_000e18;
+    uint256 internal constant SUSDS_DEPOSIT_AMOUNT = 8_000_000e18;
+    uint256 internal constant USDS_BRIDGE_AMOUNT   = 1_000_000e18;
+
+    address internal constant BASE_PAYLOAD = address(0);
+
+    function collateralsUpdates() public pure override returns (IEngine.CollateralUpdate[] memory) {
+        // Reduce LT from 70% to 65%
+        IEngine.CollateralUpdate[] memory updates = new IEngine.CollateralUpdate[](1);
+        updates[0] = IEngine.CollateralUpdate({
+            asset:          Ethereum.WBTC,
+            ltv:            EngineFlags.KEEP_CURRENT,
+            liqThreshold:   65_00,
+            liqBonus:       EngineFlags.KEEP_CURRENT,
+            debtCeiling:    EngineFlags.KEEP_CURRENT,
+            liqProtocolFee: EngineFlags.KEEP_CURRENT,
+            eModeCategory:  EngineFlags.KEEP_CURRENT
+        });
+        return updates;
+    }
 
     function _postExecute() internal override {
+        // --- Increase Morpho PT sUSDe Dec Supply Cap ---
+        IMetaMorpho(Ethereum.MORPHO_VAULT_DAI_1).submitCap(
+            MarketParams({
+                loanToken:       Ethereum.DAI,
+                collateralToken: PT_SUSDE_26DEC2024,
+                oracle:          PT_26DEC2024_PRICE_FEED,
+                irm:             Ethereum.MORPHO_DEFAULT_IRM,
+                lltv:            0.915e18
+            }),
+            200_000_000e18
+        );
+
+        // --- Adjust WETH Slope 1 Spread ---
+        LISTING_ENGINE.POOL_CONFIGURATOR().setReserveInterestRateStrategyAddress(
+            Ethereum.WETH,
+            WETH_IRM
+        );
+
+        // --- Adjust wstETH Borrow Rate Limits ---
+        ICapAutomator(Ethereum.CAP_AUTOMATOR).setBorrowCapConfig({
+            asset:            Ethereum.WSTETH,
+            max:              10_000,
+            gap:              2_000,
+            increaseCooldown: 12 hours
+        });
+
         // --- Activate Mainnet Controller ---
         RateLimitData memory rateLimitData18 = RateLimitData({
             maxAmount : 1_000_000e18,
