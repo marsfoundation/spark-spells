55b1573db400dc3a35d26573dccc231bd81c4d0b
teddy <teddy@defi.sucks>
2024-12-19 18:56:31 -0300

    refactor: delete outdated spell templates

--- test/SparkEthereum_Empty.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0
-pragma solidity ^0.8.10;
-
-import { SparkPayloadEthereum } from '../src/SparkPayloadEthereum.sol';
-
-contract SparkEthereum_Empty is SparkPayloadEthereum {
-}
