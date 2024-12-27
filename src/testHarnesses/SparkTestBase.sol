a7f88ff98f39f79683075dc19281292785f6a0fd
teddy <teddy@defi.sucks>
2024-12-27 15:55:03 -0300

    refactor: move CommonSpellAssertions to its own file

--- src/testHarnesses/SparkTestBase.sol
+++ src/testHarnesses/SparkTestBase.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.8.0;
 
 import './ProtocolV3TestBase.sol';
 
-import { Address } from 'src/libraries/Address.sol';
-
 import { InitializableAdminUpgradeabilityProxy } from "sparklend-v1-core/contracts/dependencies/openzeppelin/upgradeability/InitializableAdminUpgradeabilityProxy.sol";
 import { IACLManager }                           from 'sparklend-v1-core/contracts/interfaces/IACLManager.sol';
 import { IPoolAddressesProviderRegistry }        from 'sparklend-v1-core/contracts/interfaces/IPoolAddressesProviderRegistry.sol';
@@ -27,6 +25,7 @@ import { Domain, DomainHelpers } from "xchain-helpers/testing/Domain.sol";
 import { ChainIdUtils, ChainId } from "src/libraries/ChainId.sol";
 
 import { SpellRunner } from './SpellRunner.sol';
+import { CommonSpellAssertions } from './CommonSpellAssertions.sol';
 
 // REPO ARCHITECTURE TODOs
 // TODO: Refactor Mock logic for executor to be more realistic, consider fork + prank.
@@ -41,75 +40,6 @@ interface IAuthority {
 
 interface IExecutable {
     function execute() external;
-}
-
-
-
-/// @dev assertions that make sense to run on every chain where a spark spell
-/// can be executed
-abstract contract CommonSpellAssertions is SpellRunner {
-    function test_ETHEREUM_PayloadBytecodeMatches() public {
-        _assertPayloadBytecodeMatches(ChainIdUtils.Ethereum());
-    }
-
-    function test_BASE_PayloadBytecodeMatches() public {
-        _assertPayloadBytecodeMatches(ChainIdUtils.Base());
-    }
-
-    function test_GNOSIS_PayloadBytecodeMatches() public {
-        _assertPayloadBytecodeMatches(ChainIdUtils.Gnosis());
-    }
-
-    function _assertPayloadBytecodeMatches(ChainId chainId) private onChain(chainId) {
-        address actualPayload = chainSpellMetadata[chainId].payload;
-        vm.skip(actualPayload == address(0));
-        require(Address.isContract(actualPayload), "PAYLOAD IS NOT A CONTRACT");
-        address expectedPayload = deployPayload(chainId);
-
-        uint256 expectedBytecodeSize = expectedPayload.code.length;
-        uint256 actualBytecodeSize   = actualPayload.code.length;
-
-        uint256 metadataLength = _getBytecodeMetadataLength(expectedPayload);
-        assertTrue(metadataLength <= expectedBytecodeSize);
-        expectedBytecodeSize -= metadataLength;
-
-        metadataLength = _getBytecodeMetadataLength(actualPayload);
-        assertTrue(metadataLength <= actualBytecodeSize);
-        actualBytecodeSize -= metadataLength;
-
-        assertEq(actualBytecodeSize, expectedBytecodeSize);
-
-        uint256 size = actualBytecodeSize;
-        uint256 expectedHash;
-        uint256 actualHash;
-
-        assembly {
-            let ptr := mload(0x40)
-
-            extcodecopy(expectedPayload, ptr, 0, size)
-            expectedHash := keccak256(ptr, size)
-
-            extcodecopy(actualPayload, ptr, 0, size)
-            actualHash := keccak256(ptr, size)
-        }
-
-        assertEq(actualHash, expectedHash);
-    }
-
-    function _getBytecodeMetadataLength(address a) internal view returns (uint256 length) {
-        // The Solidity compiler encodes the metadata length in the last two bytes of the contract bytecode.
-        assembly {
-            let ptr  := mload(0x40)
-            let size := extcodesize(a)
-            if iszero(lt(size, 2)) {
-                extcodecopy(a, ptr, sub(size, 2), 2)
-                length := mload(ptr)
-                length := shr(240, length)
-                length := add(length, 2)  // The two bytes used to specify the length are not counted in the length
-            }
-            // Return zero if the bytecode is shorter than two bytes.
-        }
-    }
 }
 
 /// @dev assertions specific to sparklend, which are not run on chains where
