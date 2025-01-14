a5ba1591a1470dd26b4ab2da565847f4a7607b27
teddy <teddy@defi.sucks>
2025-01-14 12:30:14 -0300

    test: tests for Sparklend USDC AToken onboarding to SLL

--- src/proposals/20250123/SparkEthereum_20250123.t.sol
+++ src/proposals/20250123/SparkEthereum_20250123.t.sol
@@ -11,6 +11,7 @@ import { ChainIdUtils }  from 'src/libraries/ChainId.sol';
 
 contract SparkEthereum_20250123Test is SparkTestBase {
     address constant public AAVE_PRIME_USDS_ATOKEN = 0x09AA30b182488f769a9824F15E6Ce58591Da4781;
+    address constant public SPARKLEND_USDC_ATOKEN  = 0x377C3bd93f2a2984E1E7bE6A5C22c525eD4A4815;
     
     constructor() {
         id = '20250123';
@@ -29,7 +30,60 @@ contract SparkEthereum_20250123Test is SparkTestBase {
 
     function test_ETHEREUM_SLL_USDSRateLimits() public onChain(ChainIdUtils.Ethereum()) {}
 
-    function test_ETHEREUM_SLL_USDCRateLimits() public onChain(ChainIdUtils.Ethereum()) {}
+    function test_ETHEREUM_SLL_USDCRateLimits() public onChain(ChainIdUtils.Ethereum()) {
+        MainnetController controller = MainnetController(Ethereum.ALM_CONTROLLER);
+        IRateLimits rateLimits       = IRateLimits(Ethereum.ALM_RATE_LIMITS);
+        uint256 depositAmount        = 5_000_000e6;
+        deal(Ethereum.USDC, Ethereum.ALM_PROXY, 20 * depositAmount);
+
+        bytes32 depositKey = RateLimitHelpers.makeAssetKey(
+            controller.LIMIT_AAVE_DEPOSIT(),
+            SPARKLEND_USDC_ATOKEN
+        );
+
+        bytes32 withdrawKey = RateLimitHelpers.makeAssetKey(
+            controller.LIMIT_AAVE_WITHDRAW(),
+            SPARKLEND_USDC_ATOKEN
+        );
+
+        assertEq(rateLimits.getCurrentRateLimit(depositKey),  0);
+        assertEq(rateLimits.getCurrentRateLimit(withdrawKey), 0);
+        vm.prank(Ethereum.ALM_RELAYER);
+        vm.expectRevert("RateLimits/zero-maxAmount");
+        controller.depositAave(SPARKLEND_USDC_ATOKEN, depositAmount);
+
+        executeAllPayloadsAndBridges();
+
+        _assertRateLimit(depositKey, 20_000_000e6, uint256(10_000_000e6) / 1 days);
+        _assertRateLimit(withdrawKey, type(uint256).max, 0);
+
+        vm.prank(Ethereum.ALM_RELAYER);
+        vm.expectRevert("RateLimits/rate-limit-exceeded");
+        controller.depositAave(SPARKLEND_USDC_ATOKEN, 20_000_001e6);
+
+        assertEq(rateLimits.getCurrentRateLimit(depositKey),  20_000_000e6);
+        assertEq(rateLimits.getCurrentRateLimit(withdrawKey), type(uint256).max);
+
+        vm.prank(Ethereum.ALM_RELAYER);
+        controller.depositAave(SPARKLEND_USDC_ATOKEN, depositAmount);
+
+        assertEq(rateLimits.getCurrentRateLimit(depositKey),  20_000_000e6 - depositAmount);
+        assertEq(rateLimits.getCurrentRateLimit(withdrawKey), type(uint256).max);
+
+        vm.prank(Ethereum.ALM_RELAYER);
+        controller.withdrawAave(SPARKLEND_USDC_ATOKEN, depositAmount);
+
+        assertEq(rateLimits.getCurrentRateLimit(depositKey),  20_000_000e6 - depositAmount);
+        assertEq(rateLimits.getCurrentRateLimit(withdrawKey), type(uint256).max);
+
+        // slope is 10M/day, the deposit amount of 5M should be replenished in half a day.
+        // we wait for half of that, and assert half of the rate limit was replenished.
+        skip(1 days / 4);
+        assertApproxEqAbs(rateLimits.getCurrentRateLimit(depositKey),  20_000_000e6 - depositAmount/2, 20000);
+        // wait for 1 more second to avoid rounding issues
+        skip(1 days / 4 + 1);
+        assertEq(rateLimits.getCurrentRateLimit(depositKey),  20_000_000e6);
+    }
 
     function test_ETHEREUM_SLL_PrimeAUSDSRateLimits() public onChain(ChainIdUtils.Ethereum()) {
         MainnetController controller = MainnetController(Ethereum.ALM_CONTROLLER);
