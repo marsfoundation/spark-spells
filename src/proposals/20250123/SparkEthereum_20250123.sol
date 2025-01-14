e65e10f56f34498a962828853574c446174b1f77
teddy <teddy@defi.sucks>
2025-01-14 12:23:57 -0300

    feat: onboard Aave Prime USDS

--- src/proposals/20250123/SparkEthereum_20250123.sol
+++ src/proposals/20250123/SparkEthereum_20250123.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.25;
 
-import {
-    SparkPayloadEthereum
-} from "../../SparkPayloadEthereum.sol";
+import { Ethereum }                        from 'spark-address-registry/Ethereum.sol';
+import { RateLimitHelpers, RateLimitData } from "spark-alm-controller/src/RateLimitHelpers.sol";
+import { MainnetController }               from 'spark-alm-controller/src/MainnetController.sol';
+
+import { SparkPayloadEthereum } from "../../SparkPayloadEthereum.sol";
 
 /**
  * @title  Jan 23, 2025 Spark Ethereum Proposal
@@ -16,4 +18,14 @@ import {
  *         https://vote.makerdao.com/polling/QmU3Xu4W
  */
 contract SparkEthereum_20250123 is SparkPayloadEthereum {
+    address constant public AAVE_PRIME_USDS_ATOKEN = 0x09AA30b182488f769a9824F15E6Ce58591Da4781;
+    MainnetController controller = MainnetController(Ethereum.ALM_CONTROLLER);
+
+    function _postExecute() internal override {
+        _onboardAaveToken(
+            AAVE_PRIME_USDS_ATOKEN,
+            50_000_000e18,
+            uint256(50_000_000e18) / 1 days);
+    }
+
 }
