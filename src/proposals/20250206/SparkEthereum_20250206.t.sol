5160878e1611c06f3af02d55cc1876b6adb4da48
teddy <teddy@defi.sucks>
2025-01-27 16:10:33 -0300

    test: ethereum fluid susds onboarding

--- src/proposals/20250206/SparkEthereum_20250206.t.sol
+++ src/proposals/20250206/SparkEthereum_20250206.t.sol
@@ -2,24 +2,20 @@
 pragma solidity ^0.8.25;
 
 import { Ethereum }              from 'spark-address-registry/Ethereum.sol';
-import { Base }                  from 'spark-address-registry/Base.sol';
 import { MainnetController }     from 'spark-alm-controller/src/MainnetController.sol';
-import { ForeignController }     from 'spark-alm-controller/src/ForeignController.sol';
 import { IRateLimits }           from 'spark-alm-controller/src/interfaces/IRateLimits.sol';
 import { RateLimitHelpers }      from 'spark-alm-controller/src/RateLimitHelpers.sol';
-import { IERC20 }                from 'lib/erc20-helpers/src/interfaces/IERC20.sol';
-import { CCTPForwarder }         from "xchain-helpers/forwarders/CCTPForwarder.sol";
 import { Domain, DomainHelpers } from "xchain-helpers/testing/Domain.sol";
-import { Errors }                from 'sparklend-v1-core/contracts/protocol/libraries/helpers/Errors.sol';
-import { ReserveConfiguration }  from 'sparklend-v1-core/contracts/protocol/libraries/configuration/ReserveConfiguration.sol';
 import { DataTypes }             from 'sparklend-v1-core/contracts/protocol/libraries/types/DataTypes.sol';
 
-import { SparkTestBase, InterestStrategyValues } from 'src/SparkTestBase.sol';
-import { ChainIdUtils }                          from 'src/libraries/ChainId.sol';
-import { ReserveConfig }                         from '../../ProtocolV3TestBase.sol';
+import { SparkTestBase } from 'src/SparkTestBase.sol';
+import { ChainIdUtils }  from 'src/libraries/ChainId.sol';
+import { ReserveConfig } from '../../ProtocolV3TestBase.sol';
 
 contract SparkEthereum_20250206Test is SparkTestBase {
     using DomainHelpers for Domain;
+
+    address public immutable FLUID_SUSDS_VAULT      = 0x2BBE31d63E6813E3AC858C04dae43FB2a72B0D11;
 
     constructor() {
         id = '20250206';
@@ -35,12 +31,57 @@ contract SparkEthereum_20250206Test is SparkTestBase {
         deployPayloads();
     }
 
-    function test_ETHEREUM_SLL_FluidsUSDSOnboardingSideEffects() public onChain(ChainIdUtils.Ethereum()) {
-        executeAllPayloadsAndBridges();
-    }
+    function test_ETHEREUM_SLL_FluidsUSDSOnboarding() public onChain(ChainIdUtils.Ethereum()) {
+        loadPoolContext(_getPoolAddressesProviderRegistry().getAddressesProvidersList()[0]);
+        MainnetController controller = MainnetController(Ethereum.ALM_CONTROLLER);
+        IRateLimits rateLimits       = IRateLimits(Ethereum.ALM_RATE_LIMITS);
+        uint256 depositAmount        = 1_000_000e18;
 
-    function test_ETHEREUM_SLL_FluidsUSDSRateLimits() public onChain(ChainIdUtils.Ethereum()) {
+        deal(Ethereum.SUSDS, Ethereum.ALM_PROXY, 20 * depositAmount);
+        bytes32 depositKey = RateLimitHelpers.makeAssetKey(
+            controller.LIMIT_4626_DEPOSIT(),
+            FLUID_SUSDS_VAULT
+        );
+        bytes32 withdrawKey = RateLimitHelpers.makeAssetKey(
+            controller.LIMIT_4626_WITHDRAW(),
+            FLUID_SUSDS_VAULT
+        );
+
+        vm.prank(Ethereum.ALM_RELAYER);
+        vm.expectRevert("RateLimits/zero-maxAmount");
+        controller.depositERC4626(FLUID_SUSDS_VAULT, depositAmount);
+
         executeAllPayloadsAndBridges();
+
+        _assertRateLimit(depositKey, 10_000_000e18, uint256(5_000_000e18) / 1 days);
+        _assertRateLimit(withdrawKey, type(uint256).max, 0);
+
+        vm.prank(Ethereum.ALM_RELAYER);
+        vm.expectRevert("RateLimits/rate-limit-exceeded");
+        controller.depositERC4626(FLUID_SUSDS_VAULT, 10_000_001e18);
+
+        assertEq(rateLimits.getCurrentRateLimit(depositKey),  10_000_000e18);
+        assertEq(rateLimits.getCurrentRateLimit(withdrawKey), type(uint256).max);
+
+        vm.prank(Ethereum.ALM_RELAYER);
+        controller.depositERC4626(FLUID_SUSDS_VAULT, depositAmount);
+
+        assertEq(rateLimits.getCurrentRateLimit(depositKey),  10_000_000e18 - depositAmount);
+        assertEq(rateLimits.getCurrentRateLimit(withdrawKey), type(uint256).max);
+
+        vm.prank(Ethereum.ALM_RELAYER);
+        controller.withdrawERC4626(FLUID_SUSDS_VAULT, depositAmount);
+
+        assertEq(rateLimits.getCurrentRateLimit(depositKey),  10_000_000e18 - depositAmount);
+        assertEq(rateLimits.getCurrentRateLimit(withdrawKey), type(uint256).max);
+
+        // Slope is 5M/day, the deposit amount of 1M should be replenished in a fifth of a day.
+        // Wait for half of that, and assert half of the rate limit was replenished.
+        skip(1 days / 10);
+        assertApproxEqAbs(rateLimits.getCurrentRateLimit(depositKey), 10_000_000e18 - depositAmount/2, 5000);
+        // Wait for 1 more second to avoid rounding issues
+        skip(1 days / 10 + 1);
+        assertEq(rateLimits.getCurrentRateLimit(depositKey), 10_000_000e18);
     }
 
     function test_ETHEREUM_Sparklend_WBTCLiquidationThreshold() public onChain(ChainIdUtils.Ethereum()) {
